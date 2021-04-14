e94d316d2e1af5b4abb3be7884c7496c20b6cf3b
Thomas <4159106+dtslvr@users.noreply.github.com>
2021-04-14 23:44:33 +0200

    Migrate links to RouterLink

--- apps/client/src/app/components/no-transactions-info/no-transactions-info.module.ts
+++ apps/client/src/app/components/no-transactions-info/no-transactions-info.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 import { NoTransactionsInfoComponent } from './no-transactions-info.component';
 
 @NgModule({
   declarations: [NoTransactionsInfoComponent],
   exports: [NoTransactionsInfoComponent],
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, RouterModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
