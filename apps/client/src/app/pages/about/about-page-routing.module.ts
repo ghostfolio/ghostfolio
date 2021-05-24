ad961f303958de9739af497099696efc6934aad6
Thomas <4159106+dtslvr@users.noreply.github.com>
2021-05-24 16:28:42 +0200

    Bugfix/fix missing header of public pages (#121)
    * Fix missing header of public pages
    
    * Update changelog

--- apps/client/src/app/pages/about/about-page-routing.module.ts
+++ apps/client/src/app/pages/about/about-page-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@ghostfolio/client/core/auth.guard';
 
 import { AboutPageComponent } from './about-page.component';
 
-const routes: Routes = [{ path: '', component: AboutPageComponent }];
+const routes: Routes = [
+  { path: '', component: AboutPageComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- apps/client/src/app/pages/pricing/pricing-page-routing.module.ts
+++ apps/client/src/app/pages/pricing/pricing-page-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@ghostfolio/client/core/auth.guard';
 
 import { PricingPageComponent } from './pricing-page.component';
 
-const routes: Routes = [{ path: '', component: PricingPageComponent }];
+const routes: Routes = [
+  { path: '', component: PricingPageComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
