6062bdbeab26f544c79d5a34c6fce1d3df0849f1
Thomas <4159106+dtslvr@users.noreply.github.com>
2021-06-02 20:12:52 +0200

    Feature/align pricing page with subscription model (#135)
    * Align pricing page with subscription model
    
    * Update changelog

--- libs/common/src/lib/interfaces/user-with-settings.ts
+++ libs/common/src/lib/interfaces/user-with-settings.ts
@@ -1,6 +1,11 @@
+import { SubscriptionType } from '@ghostfolio/common/types/subscription.type';
 import { Account, Settings, User } from '@prisma/client';
 
 export type UserWithSettings = User & {
   Account: Account[];
   Settings: Settings;
+  subscription?: {
+    expiresAt?: Date;
+    type: SubscriptionType;
+  };
 };
