cdb1690145055f9be3385495419f55bf21a90578
Thomas <4159106+dtslvr@users.noreply.github.com>
2021-06-15 20:59:09 +0200

    Upgrade to Angular 12

--- apps/client/jest.config.js
+++ apps/client/jest.config.js
@@ -16,8 +16,8 @@ module.exports = {
   },
   coverageDirectory: '../../coverage/apps/client',
   snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment'
   ]
 };
--- libs/common/jest.config.js
+++ libs/common/jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   displayName: 'common',
   preset: '../../jest.preset.js',
   globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json'
-    }
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' }
   },
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest'
