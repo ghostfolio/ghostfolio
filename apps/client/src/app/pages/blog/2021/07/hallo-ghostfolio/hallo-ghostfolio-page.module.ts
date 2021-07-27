08a02351ca419388112f948a2e67d447827d86fb
Thomas <4159106+dtslvr@users.noreply.github.com>
2021-07-27 20:43:44 +0200

    Add link

--- apps/client/src/app/pages/blog/2021/07/hallo-ghostfolio/hallo-ghostfolio-page.module.ts
+++ apps/client/src/app/pages/blog/2021/07/hallo-ghostfolio/hallo-ghostfolio-page.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { HalloGhostfolioPageRoutingModule } from './hallo-ghostfolio-page-routing.module';
 import { HalloGhostfolioPageComponent } from './hallo-ghostfolio-page.component';
@@ -7,7 +8,7 @@ import { HalloGhostfolioPageComponent } from './hallo-ghostfolio-page.component'
 @NgModule({
   declarations: [HalloGhostfolioPageComponent],
   exports: [],
-  imports: [CommonModule, HalloGhostfolioPageRoutingModule],
+  imports: [CommonModule, HalloGhostfolioPageRoutingModule, RouterModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
