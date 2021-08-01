215cd78c8017d8898d06a99bb9d07c0be53cf4de
Thomas <4159106+dtslvr@users.noreply.github.com>
2021-08-01 17:25:03 +0200

    Remove symbol icon from dialog header

--- apps/client/src/app/components/dialog-header/dialog-header.module.ts
+++ apps/client/src/app/components/dialog-header/dialog-header.module.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { GfSymbolIconModule } from '@ghostfolio/client/components/symbol-icon/symbol-icon.module';
 
 import { DialogHeaderComponent } from './dialog-header.component';
 
 @NgModule({
   declarations: [DialogHeaderComponent],
   exports: [DialogHeaderComponent],
-  imports: [CommonModule, GfSymbolIconModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
