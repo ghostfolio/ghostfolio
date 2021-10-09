af1eb82f960c91e1558d7527ee6f82d82dd7d020
Thomas <4159106+dtslvr@users.noreply.github.com>
2021-10-09 10:45:07 +0200

    Clean up

--- apps/api/src/services/data-gathering.service.ts
+++ apps/api/src/services/data-gathering.service.ts
@@ -130,10 +130,6 @@ export class DataGatheringService {
 
     const currentData = await this.dataProviderService.get(dataGatheringItems);
 
-    Object.entries(currentData).forEach((symbol) => {
-      console.log(symbol[0], symbol[1]);
-    });
-
     for (const [
       symbol,
       {
