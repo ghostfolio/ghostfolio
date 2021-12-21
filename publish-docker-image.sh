ac88142bbcbfab0e01a22278e5c078b74cb9c994
Thomas <4159106+dtslvr@users.noreply.github.com>
2021-12-21 20:21:13 +0100

    Add tag "latest" to docker image

--- publish-docker-image.sh
+++ publish-docker-image.sh
@@ -1,5 +1,5 @@
 set -xe
 echo "$DOCKER_HUB_ACCESS_TOKEN" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
-docker build -t ghostfolio/ghostfolio:$TRAVIS_TAG .
-docker push ghostfolio/ghostfolio:$TRAVIS_TAG
+docker build -t ghostfolio/ghostfolio:$TRAVIS_TAG -t ghostfolio/ghostfolio:latest .
+docker push ghostfolio/ghostfolio --all-tags
