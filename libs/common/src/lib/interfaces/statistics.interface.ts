85ca781ff2c5832f58b8565e1c4ed67d3539f95e
Thomas <4159106+dtslvr@users.noreply.github.com>
2021-12-29 18:13:34 +0100

    Extend statistics with users in Slack community

--- libs/common/src/lib/interfaces/statistics.interface.ts
+++ libs/common/src/lib/interfaces/statistics.interface.ts
@@ -1,8 +1,8 @@
 export interface Statistics {
   activeUsers1d: number;
-  activeUsers7d: number;
   activeUsers30d: number;
   gitHubContributors: number;
   gitHubStargazers: number;
   newUsers30d: number;
+  slackCommunityUsers: string;
 }
