ba0d17c3239fc56275cca2ef91aa7acb2bd5ead7
Thomas <4159106+dtslvr@users.noreply.github.com>
2022-04-18 18:09:57 +0200

    Reuse value component

--- apps/client/src/app/pages/about/about-page.module.ts
+++ apps/client/src/app/pages/about/about-page.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { GfValueModule } from '@ghostfolio/ui/value';
 
 import { AboutPageRoutingModule } from './about-page-routing.module';
 import { AboutPageComponent } from './about-page.component';
@@ -12,6 +13,7 @@ import { AboutPageComponent } from './about-page.component';
   imports: [
     AboutPageRoutingModule,
     CommonModule,
+    GfValueModule,
     MatButtonModule,
     MatCardModule
   ],
