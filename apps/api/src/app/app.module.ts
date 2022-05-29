1bf9fd0e172277f888b4c94b23cabb16f4b8b109
willyp713 <willyp713@users.noreply.github.com>
2022-05-28 21:07:01 -0500

    Update app.module.ts

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -41,7 +41,7 @@ import { UserModule } from './user/user.module';
       redis: {
         host: process.env.REDIS_HOST,
         port: parseInt(process.env.REDIS_PORT, 10),
-        password: process.env.REDIS_PASSWORD,
+        password: process.env.REDIS_PASSWORD
       }
     }),
     CacheModule,
