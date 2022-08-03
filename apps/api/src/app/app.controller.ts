3bd7bf49e81af93080d503883f5d2cfe573b1053
Thomas <4159106+dtslvr@users.noreply.github.com>
2022-08-03 21:34:07 +0200

    Fix database connection error handling

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,6 +1,20 @@
+import { ExchangeRateDataService } from '@ghostfolio/api/services/exchange-rate-data.service';
+import { PrismaService } from '@ghostfolio/api/services/prisma.service';
 import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  public constructor() {}
+  public constructor(
+    private readonly exchangeRateDataService: ExchangeRateDataService,
+    private readonly prismaService: PrismaService
+  ) {
+    this.initialize();
+  }
+
+  private async initialize() {
+    try {
+      await this.prismaService.$connect();
+      await this.exchangeRateDataService.initialize();
+    } catch {}
+  }
 }
