90720da6c28db9dd7311eeb6075f5edae1060135
Thomas <4159106+dtslvr@users.noreply.github.com>
2022-08-08 20:09:30 +0200

    Harmonize benchmark table styling

--- libs/ui/src/lib/benchmark/benchmark.module.ts
+++ libs/ui/src/lib/benchmark/benchmark.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 import { GfValueModule } from '../value';
@@ -8,7 +9,12 @@ import { BenchmarkComponent } from './benchmark.component';
 @NgModule({
   declarations: [BenchmarkComponent],
   exports: [BenchmarkComponent],
-  imports: [CommonModule, GfValueModule, NgxSkeletonLoaderModule],
+  imports: [
+    CommonModule,
+    GfValueModule,
+    MatTableModule,
+    NgxSkeletonLoaderModule
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class GfBenchmarkModule {}
