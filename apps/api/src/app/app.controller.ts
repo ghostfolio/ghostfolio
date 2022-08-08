4ccabde251496ed031338231e84bd076572212f1
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2022-08-08 19:25:38 +0200

    Feature/simplify exchange rate service initialization (#1128)
    * Simplify initialization
    
    * Update changelog

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,19 +1,16 @@
 import { ExchangeRateDataService } from '@ghostfolio/api/services/exchange-rate-data.service';
-import { PrismaService } from '@ghostfolio/api/services/prisma.service';
 import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   public constructor(
-    private readonly exchangeRateDataService: ExchangeRateDataService,
-    private readonly prismaService: PrismaService
+    private readonly exchangeRateDataService: ExchangeRateDataService
   ) {
     this.initialize();
   }
 
   private async initialize() {
     try {
-      await this.prismaService.$connect();
       await this.exchangeRateDataService.initialize();
     } catch {}
   }
