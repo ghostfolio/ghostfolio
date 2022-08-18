6c4138c4657c6bd12e3940dac6a5fbacbe70467d
Thomas <4159106+dtslvr@users.noreply.github.com>
2022-08-18 21:13:12 +0200

    Resolve feature graphic of blog post

--- apps/api/src/app/frontend.middleware.ts
+++ apps/api/src/app/frontend.middleware.ts
@@ -1,13 +1,33 @@
 import * as path from 'path';
 
 import { DEFAULT_LANGUAGE_CODE } from '@ghostfolio/common/config';
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class FrontendMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    if (req.path.startsWith('/api/') || this.isFileRequest(req.url)) {
+    if (req.url.includes('cover.png')) {
+      Logger.log(`Referer: ${req.headers.referer}`, 'FrontendMiddleware');
+
+      // Resolve feature graphic for blog post
+      if (req.headers.referer?.includes('500-stars-on-github')) {
+        res.sendFile(
+          path.join(
+            __dirname,
+            '..',
+            'client',
+            'assets',
+            'images',
+            'blog',
+            '500-stars-on-github.jpg'
+          )
+        );
+      } else {
+        // Skip
+        next();
+      }
+    } else if (req.path.startsWith('/api/') || this.isFileRequest(req.url)) {
       // Skip
       next();
     } else if (req.path.startsWith('/de/')) {
