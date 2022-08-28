01d4ae8757e9a9bf24afa29450d6b9eb5ce9e0d0
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2022-08-28 21:10:25 +0200

    Feature/move build pipeline from travis to GitHub actions (#1203)
    * Remove travis configurations
    
    * Update changelog

--- publish-docker-image.sh
+++
@@ -1,5 +0,0 @@
-set -xe
-echo "$DOCKER_HUB_ACCESS_TOKEN" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
-
-docker build -t ghostfolio/ghostfolio:$TRAVIS_TAG -t ghostfolio/ghostfolio:latest .
-docker push ghostfolio/ghostfolio --all-tags
