8aa87418298cdc666ee0a2796c5a84daa9320504
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2022-10-08 17:35:18 +0200

    Feature/simplify admin settings management (#1340)
    * Simplify settings management
    
    * Update changelog

--- apps/api/src/services/property/property.dto.ts
+++ apps/api/src/services/property/property.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class PropertyDto {
+  @IsOptional()
   @IsString()
   value: string;
 }
