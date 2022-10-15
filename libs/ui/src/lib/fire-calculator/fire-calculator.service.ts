246de7aa86daa5c45628329be2ab3831922007b8
Yash Solanki <yashsolanki1709@gmail.com>
2022-10-15 14:15:11 +0530

    Consider current month in FIRE calculation (#1349)

--- libs/ui/src/lib/fire-calculator/fire-calculator.service.ts
+++ libs/ui/src/lib/fire-calculator/fire-calculator.service.ts
@@ -4,35 +4,32 @@ import Big from 'big.js';
 @Injectable()
 export class FireCalculatorService {
   private readonly COMPOUND_PERIOD = 12;
-  private readonly CONTRIBUTION_PERIOD = 12;
 
   public constructor() {}
 
   public calculateCompoundInterest({
     P,
-    period,
+    periodInMonths,
     PMT,
     r
   }: {
     P: number;
-    period: number;
+    periodInMonths: number;
     PMT: number;
     r: number;
   }) {
     let interest = new Big(0);
-    const principal = new Big(P).plus(
-      new Big(PMT).mul(this.CONTRIBUTION_PERIOD).mul(period)
-    );
+    const principal = new Big(P).plus(new Big(PMT).mul(periodInMonths));
     let totalAmount = principal;
 
     if (r) {
       const compoundInterestForPrincipal = new Big(1)
         .plus(new Big(r).div(this.COMPOUND_PERIOD))
-        .pow(new Big(this.COMPOUND_PERIOD).mul(period).toNumber());
+        .pow(periodInMonths);
       const compoundInterest = new Big(P).mul(compoundInterestForPrincipal);
       const contributionInterest = new Big(
         new Big(PMT).mul(compoundInterestForPrincipal.minus(1))
-      ).div(new Big(r).div(this.CONTRIBUTION_PERIOD));
+      ).div(new Big(r).div(this.COMPOUND_PERIOD));
       interest = compoundInterest.plus(contributionInterest).minus(principal);
       totalAmount = compoundInterest.plus(contributionInterest);
     }
