f53813720a9ed1d3fa50cbe351d488713c13f8ea
Thomas <4159106+dtslvr@users.noreply.github.com>
2022-11-04 22:22:34 +0100

    Improve usage of premium indicator component

--- apps/client/src/app/pages/portfolio/report/report-page.module.ts
+++ apps/client/src/app/pages/portfolio/report/report-page.module.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RulesModule } from '@ghostfolio/client/components/rules/rules.module';
+import { GfPremiumIndicatorModule } from '@ghostfolio/ui/premium-indicator';
 
 import { ReportPageRoutingModule } from './report-page-routing.module';
 import { ReportPageComponent } from './report-page.component';
 
 @NgModule({
   declarations: [ReportPageComponent],
-  imports: [CommonModule, ReportPageRoutingModule, RulesModule],
+  imports: [
+    CommonModule,
+    GfPremiumIndicatorModule,
+    ReportPageRoutingModule,
+    RulesModule
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ReportPageModule {}
