fb496431e8142f0ee4e34f2a6fd02467ca848abe
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2022-11-27 10:21:21 +0100

    Clean up (#1481)

--- apps/client/src/app/pages/blog/2022/11/black-friday-2022/black-friday-2022-page.component.ts
+++ apps/client/src/app/pages/blog/2022/11/black-friday-2022/black-friday-2022-page.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DataService } from '@ghostfolio/client/services/data.service';
 
 @Component({
   host: { class: 'page' },
@@ -8,14 +7,5 @@ import { DataService } from '@ghostfolio/client/services/data.service';
   templateUrl: './black-friday-2022-page.html'
 })
 export class BlackFriday2022PageComponent {
-  public discount: number;
-
-  public constructor(private dataService: DataService) {
-    const { subscriptions } = this.dataService.fetchInfo();
-
-    const coupon = subscriptions?.[0]?.coupon ?? 0;
-    const price = subscriptions?.[0]?.price ?? 1;
-
-    this.discount = Math.floor((coupon / price) * 100) / 100;
-  }
+  public constructor() {}
 }
