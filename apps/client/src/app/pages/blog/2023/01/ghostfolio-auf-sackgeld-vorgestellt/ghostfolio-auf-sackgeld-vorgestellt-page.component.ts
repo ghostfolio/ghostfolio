93b24a7a4002cc9079924cc02ee46eadc83e43eb
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-01-21 15:54:41 +0100

    Add blog post: Ghostfolio auf Sackgeld.com vorgestellt

---
+++ apps/client/src/app/pages/blog/2023/01/ghostfolio-auf-sackgeld-vorgestellt/ghostfolio-auf-sackgeld-vorgestellt-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  host: { class: 'page' },
+  selector: 'gf-ghostfolio-auf-sackgeld-vorgestellt-page',
+  styleUrls: ['./ghostfolio-auf-sackgeld-vorgestellt-page.scss'],
+  templateUrl: './ghostfolio-auf-sackgeld-vorgestellt-page.html'
+})
+export class GhostfolioAufSackgeldVorgestelltPageComponent {}
