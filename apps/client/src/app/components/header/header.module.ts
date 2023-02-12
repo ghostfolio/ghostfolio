92a6c93c5b9dbbd04ed4ae0d985f24a7691baad7
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-02-12 21:31:46 +0100

    Migrate GfHeaderModule to Angular Material 15

--- apps/client/src/app/components/header/header.module.ts
+++ apps/client/src/app/components/header/header.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { LoginWithAccessTokenDialogModule } from '@ghostfolio/client/components/login-with-access-token-dialog/login-with-access-token-dialog.module';
