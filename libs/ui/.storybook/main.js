ce022c024f535fb3cab9332597311de06d2298a7
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2023-02-18 19:34:06 +0100

    Feature/upgrade nx to version 15.7.2 (#1721)
    * Upgrade Angular and Nx
    
    * Update changelog

--- libs/ui/.storybook/main.js
+++ libs/ui/.storybook/main.js
@@ -10,7 +10,7 @@ module.exports = {
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  addons: [...rootMain.addons],
+  addons: ['@storybook/addon-essentials', ...rootMain.addons],
   webpackFinal: async (config, { configType }) => {
     // apply any global webpack configs that might have been specified in .storybook/main.js
     if (rootMain.webpackFinal) {
