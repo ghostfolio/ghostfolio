a58206f6cb84845e8eb0831ff29577a251fe1416
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-03-31 20:39:07 +0200

    Improve handling of platforms

---
+++ apps/api/src/services/platform/platform.service.ts
@@ -0,0 +1,11 @@
+import { PrismaService } from '@ghostfolio/api/services/prisma.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PlatformService {
+  public constructor(private readonly prismaService: PrismaService) {}
+
+  public async get() {
+    return this.prismaService.platform.findMany();
+  }
+}
