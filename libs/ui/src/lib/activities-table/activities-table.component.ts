5dda728fd616ffc1bc64c0228740e42253d21784
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-04-07 16:39:01 +0200

    Format code

--- libs/ui/src/lib/activities-table/activities-table.component.ts
+++ libs/ui/src/lib/activities-table/activities-table.component.ts
@@ -10,10 +10,7 @@ import {
   Output,
   ViewChild
 } from '@angular/core';
-import {
-  MatPaginator,
-  PageEvent
-} from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
