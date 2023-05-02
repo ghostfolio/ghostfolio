8ae255ff9741fc428d84d85f5c82ffec8d8878ab
Fabio Azevedo (CTW) <Fabio.AM.Azevedo@ctw.bmwgroup.com>
2023-05-02 19:45:47 +0100

    Added missing imports

--- apps/client/src/app/components/platform/create-or-update-platform-dialog/create-or-update-platform-dialog.module.ts
+++ apps/client/src/app/components/platform/create-or-update-platform-dialog/create-or-update-platform-dialog.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateOrUpdatePlatformDialog } from './create-or-update-account-platform.component';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [CreateOrUpdatePlatformDialog],
-  imports: [ReactiveFormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ]
 })
 export class GfCreateOrUpdatePlatformDialogModule {}
