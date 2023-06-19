66002a0a0f3eab30a6d51d19c04c7fad3f857a39
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-06-19 20:27:01 +0200

    Clean up

--- apps/client/src/app/components/admin-market-data/create-asset-profile-dialog/interfaces/interfaces.ts
+++ apps/client/src/app/components/admin-market-data/create-asset-profile-dialog/interfaces/interfaces.ts
@@ -1,7 +1,4 @@
-import { AdminMarketDataItem } from '@ghostfolio/common/interfaces';
-
 export interface CreateAssetProfileDialogParams {
-  existingSymbols: AdminMarketDataItem[];
   deviceType: string;
   locale: string;
 }
