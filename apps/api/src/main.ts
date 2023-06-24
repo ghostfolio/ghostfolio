13b016ea7a7559bd97f6d784a481fb0bf3abfc8b
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-06-24 18:04:51 +0200

    Release 1.283.2

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -39,7 +39,9 @@ async function bootstrap() {
     helmet({
       contentSecurityPolicy: {
         directives: {
-          scriptSrc: ["'self'", "'unsafe-inline'"] // Allow inline scripts / styles
+          scriptSrc: ["'self'", "'unsafe-inline'"], // Allow inline scripts
+          scriptSrcAttr: ["'self'", "'unsafe-inline'"], // Allow inline event handlers
+          styleSrc: ["'self'", "'unsafe-inline'"] // Allow inline styles
         }
       }
     })
