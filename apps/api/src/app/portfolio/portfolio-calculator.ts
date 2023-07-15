c069c0ffa4f0df1a2c1cfe07a1248356106a353b
Daniel Devaud <devaudan@students.zhaw.ch>
2023-07-15 12:11:23 +0200

    Refactored performance calculations
    - Avoiding multiple iterations over same array
    - Some Refactorings for readability

--- apps/api/src/app/portfolio/portfolio-calculator.ts
+++ apps/api/src/app/portfolio/portfolio-calculator.ts
@@ -3,6 +3,7 @@ import { IDataGatheringItem } from '@ghostfolio/api/services/interfaces/interfac
 import { DATE_FORMAT, parseDate, resetHours } from '@ghostfolio/common/helper';
 import {
   DataProviderInfo,
+  HistoricalDataItem,
   ResponseError,
   TimelinePosition
 } from '@ghostfolio/common/interfaces';
@@ -277,46 +278,29 @@ export class PortfolioCalculator {
       };
     }
 
-    for (const currentDate of dates) {
-      const dateString = format(currentDate, DATE_FORMAT);
+    return dates.map((date) => {
+      const dateString = format(date, DATE_FORMAT);
+      let totalCurrentValue = new Big(0);
+      let totalInvestmentValue = new Big(0);
+      let maxTotalInvestmentValue = new Big(0);
+      let totalNetPerformanceValue = new Big(0);
 
       for (const symbol of Object.keys(valuesBySymbol)) {
         const symbolValues = valuesBySymbol[symbol];
 
-        const currentValue =
-          symbolValues.currentValues?.[dateString] ?? new Big(0);
-        const investmentValue =
-          symbolValues.investmentValues?.[dateString] ?? new Big(0);
-        const maxInvestmentValue =
-          symbolValues.maxInvestmentValues?.[dateString] ?? new Big(0);
-        const netPerformanceValue =
-          symbolValues.netPerformanceValues?.[dateString] ?? new Big(0);
-
-        valuesByDate[dateString] = {
-          totalCurrentValue: (
-            valuesByDate[dateString]?.totalCurrentValue ?? new Big(0)
-          ).add(currentValue),
-          totalInvestmentValue: (
-            valuesByDate[dateString]?.totalInvestmentValue ?? new Big(0)
-          ).add(investmentValue),
-          maxTotalInvestmentValue: (
-            valuesByDate[dateString]?.maxTotalInvestmentValue ?? new Big(0)
-          ).add(maxInvestmentValue),
-          totalNetPerformanceValue: (
-            valuesByDate[dateString]?.totalNetPerformanceValue ?? new Big(0)
-          ).add(netPerformanceValue)
-        };
+        totalCurrentValue = totalCurrentValue.plus(
+          symbolValues.currentValues?.[dateString] ?? new Big(0)
+        );
+        totalInvestmentValue = totalInvestmentValue.plus(
+          symbolValues.investmentValues?.[dateString] ?? new Big(0)
+        );
+        maxTotalInvestmentValue = maxTotalInvestmentValue.plus(
+          symbolValues.maxInvestmentValues?.[dateString] ?? new Big(0)
+        );
+        totalNetPerformanceValue = totalNetPerformanceValue.plus(
+          symbolValues.netPerformanceValues?.[dateString] ?? new Big(0)
+        );
       }
-    }
-
-    return Object.entries(valuesByDate).map(([date, values]) => {
-      const {
-        maxTotalInvestmentValue,
-        totalCurrentValue,
-        totalInvestmentValue,
-        totalNetPerformanceValue
-      } = values;
-
       const netPerformanceInPercentage = maxTotalInvestmentValue.eq(0)
         ? 0
         : totalNetPerformanceValue
@@ -325,7 +309,7 @@ export class PortfolioCalculator {
             .toNumber();
 
       return {
-        date,
+        date: dateString,
         netPerformanceInPercentage,
         netPerformance: totalNetPerformanceValue.toNumber(),
         totalInvestment: totalInvestmentValue.toNumber(),
