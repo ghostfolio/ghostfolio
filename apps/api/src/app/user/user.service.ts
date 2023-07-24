0866587cabfeae55a21298bde2a29bf2fdfb7443
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2023-07-24 20:12:07 +0200

    Increase frequency (#2169)

--- apps/api/src/app/user/user.service.ts
+++ apps/api/src/app/user/user.service.ts
@@ -166,7 +166,7 @@ export class UserService {
         this.subscriptionService.getSubscription(Subscription);
 
       if (
-        Analytics?.activityCount % 10 === 0 &&
+        Analytics?.activityCount % 5 === 0 &&
         user.subscription?.type === 'Basic'
       ) {
         currentPermissions.push(permissions.enableSubscriptionInterstitial);
