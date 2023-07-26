efee21e6ed8797db545e8ff7c2dc9df691c842d7
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-07-26 20:36:01 +0200

    Fix RedisClient import

--- apps/api/src/app/redis-cache/interfaces/redis-store.interface.ts
+++ apps/api/src/app/redis-cache/interfaces/redis-store.interface.ts
@@ -1,8 +1,8 @@
 import { Store } from 'cache-manager';
-import Redis from 'redis';
+import { RedisClient } from 'redis';
 
 export interface RedisStore extends Store {
-  getClient: () => Redis.RedisClient;
+  getClient: () => RedisClient;
   isCacheableValue: (value: any) => boolean;
   name: 'redis';
 }
