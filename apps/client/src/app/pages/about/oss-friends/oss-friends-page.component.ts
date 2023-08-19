4729a52f25ecd6b9e4d2014f1f2d0e92aecad21b
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-08-19 08:58:03 +0200

    Remove zero width characters

--- apps/client/src/app/pages/about/oss-friends/oss-friends-page.component.ts
+++ apps/client/src/app/pages/about/oss-friends/oss-friends-page.component.ts
@@ -40,7 +40,7 @@ export class OpenSourceSoftwareFriendsPageComponent implements OnDestroy {
     },
     {
       description:
-        'The Open-Source HubSpot Alternative. A single XOS enables to create unique and life-changing experiences ​​that work for all types of business.',
+        'The Open-Source HubSpot Alternative. A single XOS enables to create unique and life-changing experiences that work for all types of business.',
       name: 'Erxes',
       url: 'https://erxes.io'
     },
