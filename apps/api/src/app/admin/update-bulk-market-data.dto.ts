7c4ca61e5b1ebb16402c46d6962a378657906418
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-10-14 10:19:41 +0200

    Reorder

--- apps/api/src/app/admin/update-bulk-market-data.dto.ts
+++ apps/api/src/app/admin/update-bulk-market-data.dto.ts
@@ -3,8 +3,8 @@ import { Type } from 'class-transformer';
 import { UpdateMarketDataDto } from './update-market-data.dto';
 
 export class UpdateBulkMarketDataDto {
-  @IsArray()
   @ArrayNotEmpty()
+  @IsArray()
   @Type(() => UpdateMarketDataDto)
   marketData: UpdateMarketDataDto[];
 }
