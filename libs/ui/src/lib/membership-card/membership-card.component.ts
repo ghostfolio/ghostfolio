7b4ffd34279eb41768b19f2ef470c6db6784e90d
Basimohd <basimohammedkt@gmail.com>
2023-10-18 15:27:05 +0530

    Created Membership-card Component

---
+++ libs/ui/src/lib/membership-card/membership-card.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'gf-membership-card',
+    styleUrls: ['./membership-card.component.scss'],
+    templateUrl: './membership-card.component.html'
+})
+export class MembershipCardComponent {
+    @Input() public expiresAt: Date;
+    @Input() public name: string;
+}
---
+++ libs/ui/src/lib/membership-card/membership-card.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+
+import { MembershipCardComponent } from './membership-card.component';
+import { GfLogoModule } from '@ghostfolio/ui/logo';
+
+@NgModule({
+    declarations: [MembershipCardComponent],
+    exports: [MembershipCardComponent],
+    imports: [CommonModule, GfLogoModule],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class GFMembershipCardModule { }
