68b20dd4eab08d399225d3c799ef2fc499bb31a1
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-11-11 18:35:56 +0100

    Introduce action menus
    * Exchange rates management
    * Coupons management

--- apps/client/src/app/components/admin-overview/admin-overview.module.ts
+++ apps/client/src/app/components/admin-overview/admin-overview.module.ts
@@ -3,6 +3,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterModule } from '@angular/router';
@@ -20,6 +21,7 @@ import { AdminOverviewComponent } from './admin-overview.component';
     GfValueModule,
     MatButtonModule,
     MatCardModule,
+    MatMenuModule,
     MatSelectModule,
     MatSlideToggleModule,
     ReactiveFormsModule,
