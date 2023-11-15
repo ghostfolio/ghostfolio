7ad2de0d2502fb30537ef197b51bdbcd84a9921c
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-11-15 20:06:53 +0100

    Refactoring

--- libs/ui/src/lib/benchmark/benchmark.component.ts
+++ libs/ui/src/lib/benchmark/benchmark.component.ts
@@ -1,16 +1,11 @@
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   Input,
   OnChanges
 } from '@angular/core';
-import { UserService } from '@ghostfolio/client/services/user/user.service';
-import { locale } from '@ghostfolio/common/config';
 import { resolveMarketCondition } from '@ghostfolio/common/helper';
 import { Benchmark, User } from '@ghostfolio/common/interfaces';
-import { without } from 'lodash';
-import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'gf-benchmark',
@@ -21,42 +16,23 @@ import { Subject, takeUntil } from 'rxjs';
 export class BenchmarkComponent implements OnChanges {
   @Input() benchmarks: Benchmark[];
   @Input() locale: string;
+  @Input() user: User;
 
   public displayedColumns = ['name', 'date', 'change', 'marketCondition'];
   public resolveMarketCondition = resolveMarketCondition;
-  public user: User;
 
-  private unsubscribeSubject = new Subject<void>();
-
-  public constructor(
-    private changeDetectorRef: ChangeDetectorRef,
-    private userService: UserService
-  ) {
-    this.userService.stateChanged
-      .pipe(takeUntil(this.unsubscribeSubject))
-      .subscribe((state) => {
-        if (state?.user) {
-          this.user = state.user;
-
-          if (this.user?.settings?.isExperimentalFeatures) {
-            this.displayedColumns = [
-              'name',
-              'trend50d',
-              'trend200d',
-              'date',
-              'change',
-              'marketCondition'
-            ];
-          }
-
-          this.changeDetectorRef.markForCheck();
-        }
-      });
-  }
+  public constructor() {}
 
   public ngOnChanges() {
-    if (!this.locale) {
-      this.locale = locale;
+    if (this.user?.settings?.isExperimentalFeatures) {
+      this.displayedColumns = [
+        'name',
+        'trend50d',
+        'trend200d',
+        'date',
+        'change',
+        'marketCondition'
+      ];
     }
   }
 }
