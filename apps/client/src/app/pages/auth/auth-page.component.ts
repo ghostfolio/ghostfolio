9db84ca11e0d83843fec2930019af55e28700897
Thomas <4159106+dtslvr@users.noreply.github.com>
2023-12-03 10:28:53 +0100

    Remove token on device registration

--- apps/client/src/app/pages/auth/auth-page.component.ts
+++ apps/client/src/app/pages/auth/auth-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {
-  STAY_SIGNED_IN,
+  KEY_STAY_SIGNED_IN,
   SettingsStorageService
 } from '@ghostfolio/client/services/settings-storage.service';
 import { TokenStorageService } from '@ghostfolio/client/services/token-storage.service';
@@ -31,7 +31,7 @@ export class AuthPageComponent implements OnDestroy, OnInit {
 
         this.tokenStorageService.saveToken(
           jwt,
-          this.settingsStorageService.getSetting(STAY_SIGNED_IN) === 'true'
+          this.settingsStorageService.getSetting(KEY_STAY_SIGNED_IN) === 'true'
         );
 
         this.router.navigate(['/']);
