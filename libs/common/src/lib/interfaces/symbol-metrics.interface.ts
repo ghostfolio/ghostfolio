4f9e3ee6b97be616aee3b05adc8d92e54ef04858
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2024-01-16 17:59:33 +0100

    Introduce type

---
+++ libs/common/src/lib/interfaces/symbol-metrics.interface.ts
@@ -0,0 +1,41 @@
+import Big from 'big.js';
+
+export interface SymbolMetrics {
+  currentValues: {
+    [date: string]: Big;
+  };
+  currentValuesWithCurrencyEffect: {
+    [date: string]: Big;
+  };
+  grossPerformance: Big;
+  grossPerformancePercentage: Big;
+  grossPerformancePercentageWithCurrencyEffect: Big;
+  grossPerformanceWithCurrencyEffect: Big;
+  hasErrors: boolean;
+  initialValue: Big;
+  initialValueWithCurrencyEffect: Big;
+  investmentValues: {
+    [date: string]: Big;
+  };
+  investmentValuesWithCurrencyEffect: {
+    [date: string]: Big;
+  };
+  netPerformance: Big;
+  netPerformancePercentage: Big;
+  netPerformancePercentageWithCurrencyEffect: Big;
+  netPerformanceValues: {
+    [date: string]: Big;
+  };
+  netPerformanceValuesWithCurrencyEffect: { [date: string]: Big };
+  netPerformanceWithCurrencyEffect: Big;
+  timeWeightedInvestment: Big;
+  timeWeightedInvestmentValues: {
+    [date: string]: Big;
+  };
+  timeWeightedInvestmentValuesWithCurrencyEffect: {
+    [date: string]: Big;
+  };
+  timeWeightedInvestmentWithCurrencyEffect: Big;
+  totalInvestment: Big;
+  totalInvestmentWithCurrencyEffect: Big;
+}
