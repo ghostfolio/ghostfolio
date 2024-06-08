7d2f31e97f7ea57d4dc06988c563cb8810578c63
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2024-06-08 19:18:11 +0200

    Improve portfolio summary

--- apps/client/src/app/components/home-summary/home-summary.module.ts
+++ apps/client/src/app/components/home-summary/home-summary.module.ts
@@ -3,18 +3,12 @@ import { GfPortfolioSummaryModule } from '@ghostfolio/client/components/portfoli
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { RouterModule } from '@angular/router';
 
 import { HomeSummaryComponent } from './home-summary.component';
 
 @NgModule({
   declarations: [HomeSummaryComponent],
-  imports: [
-    CommonModule,
-    GfPortfolioSummaryModule,
-    MatCardModule,
-    RouterModule
-  ],
+  imports: [CommonModule, GfPortfolioSummaryModule, MatCardModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class GfHomeSummaryModule {}
