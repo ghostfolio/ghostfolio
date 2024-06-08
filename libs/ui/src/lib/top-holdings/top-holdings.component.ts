8ce81518a075867487125548a9db652bb1844342
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2024-06-08 11:00:33 +0200

    Improve allocations by ETF holding

--- libs/ui/src/lib/top-holdings/top-holdings.component.ts
+++ libs/ui/src/lib/top-holdings/top-holdings.component.ts
@@ -2,6 +2,7 @@ import { getLocale } from '@ghostfolio/common/helper';
 import { Holding } from '@ghostfolio/common/interfaces';
 import { GfValueComponent } from '@ghostfolio/ui/value';
 
+import { CommonModule } from '@angular/common';
 import {
   CUSTOM_ELEMENTS_SCHEMA,
   ChangeDetectionStrategy,
@@ -13,14 +14,24 @@ import {
   ViewChild
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { get } from 'lodash';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { Subject } from 'rxjs';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [GfValueComponent, MatButtonModule, MatSortModule, MatTableModule],
+  imports: [
+    CommonModule,
+    GfValueComponent,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    NgxSkeletonLoaderModule
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'gf-top-holdings',
   standalone: true,
@@ -30,8 +41,10 @@ import { Subject } from 'rxjs';
 export class GfTopHoldingsComponent implements OnChanges, OnDestroy, OnInit {
   @Input() baseCurrency: string;
   @Input() locale = getLocale();
+  @Input() pageSize = Number.MAX_SAFE_INTEGER;
   @Input() topHoldings: Holding[];
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   public dataSource: MatTableDataSource<Holding> = new MatTableDataSource();
@@ -40,6 +53,7 @@ export class GfTopHoldingsComponent implements OnChanges, OnDestroy, OnInit {
     'valueInBaseCurrency',
     'allocationInPercentage'
   ];
+  public isLoading = true;
 
   private unsubscribeSubject = new Subject<void>();
 
@@ -48,12 +62,24 @@ export class GfTopHoldingsComponent implements OnChanges, OnDestroy, OnInit {
   public ngOnInit() {}
 
   public ngOnChanges() {
-    if (this.topHoldings) {
-      this.dataSource = new MatTableDataSource(this.topHoldings);
+    this.isLoading = true;
 
-      this.dataSource.sort = this.sort;
-      this.dataSource.sortingDataAccessor = get;
+    this.dataSource = new MatTableDataSource(this.topHoldings);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.dataSource.sortingDataAccessor = get;
+
+    if (this.topHoldings) {
+      this.isLoading = false;
     }
+  }
+
+  public onShowAllHoldings() {
+    this.pageSize = Number.MAX_SAFE_INTEGER;
+
+    setTimeout(() => {
+      this.dataSource.paginator = this.paginator;
+    });
   }
 
   public ngOnDestroy() {
