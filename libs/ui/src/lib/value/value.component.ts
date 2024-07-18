176f127dd6c585670e35e0d221b53b4c928a1dd2
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2024-07-18 20:26:34 +0200

    Improve numerical precision in holding detail dialog

--- libs/ui/src/lib/value/value.component.ts
+++ libs/ui/src/lib/value/value.component.ts
@@ -58,8 +58,8 @@ export class GfValueComponent implements OnChanges {
               this.formattedValue = this.absoluteValue.toLocaleString(
                 this.locale,
                 {
-                  maximumFractionDigits: 2,
-                  minimumFractionDigits: 2
+                  maximumFractionDigits: this.precision,
+                  minimumFractionDigits: this.precision
                 }
               );
             } catch {}
@@ -68,8 +68,8 @@ export class GfValueComponent implements OnChanges {
               this.formattedValue = (this.absoluteValue * 100).toLocaleString(
                 this.locale,
                 {
-                  maximumFractionDigits: 2,
-                  minimumFractionDigits: 2
+                  maximumFractionDigits: this.precision,
+                  minimumFractionDigits: this.precision
                 }
               );
             } catch {}
@@ -77,8 +77,8 @@ export class GfValueComponent implements OnChanges {
         } else if (this.isCurrency) {
           try {
             this.formattedValue = this.value?.toLocaleString(this.locale, {
-              maximumFractionDigits: 2,
-              minimumFractionDigits: 2
+              maximumFractionDigits: this.precision,
+              minimumFractionDigits: this.precision
             });
           } catch {}
         } else if (this.isPercent) {
@@ -86,17 +86,10 @@ export class GfValueComponent implements OnChanges {
             this.formattedValue = (this.value * 100).toLocaleString(
               this.locale,
               {
-                maximumFractionDigits: 2,
-                minimumFractionDigits: 2
+                maximumFractionDigits: this.precision,
+                minimumFractionDigits: this.precision
               }
             );
-          } catch {}
-        } else if (this.precision || this.precision === 0) {
-          try {
-            this.formattedValue = this.value?.toLocaleString(this.locale, {
-              maximumFractionDigits: this.precision,
-              minimumFractionDigits: this.precision
-            });
           } catch {}
         } else {
           this.formattedValue = this.value?.toLocaleString(this.locale);
@@ -136,6 +129,8 @@ export class GfValueComponent implements OnChanges {
     this.isNumber = false;
     this.isString = false;
     this.locale = this.locale || getLocale();
+    this.precision =
+      this.precision || this.precision === 0 ? this.precision : 2;
     this.useAbsoluteValue = false;
   }
 }
