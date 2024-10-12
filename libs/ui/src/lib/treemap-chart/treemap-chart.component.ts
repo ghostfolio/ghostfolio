8e2d2c772457ca28f07160f97a72aa725ef6a49e
tushar-agarwal7 <tushar.agarwal7373@gmail.com>
2024-10-12 08:38:55 +0000

    Adjust font sizes in treemap chart and maintain tooltip functionality

--- libs/ui/src/lib/treemap-chart/treemap-chart.component.ts
+++ libs/ui/src/lib/treemap-chart/treemap-chart.component.ts
@@ -151,16 +151,15 @@ export class GfTreemapChartComponent
             align: 'left',
             color: ['white'],
             display: true,
-            font: [{ size: 14 }, { size: 14 }, { lineHeight: 2, size: 14 }], 
+            font: { size: 14, lineHeight: 2 }, 
             formatter(ctx) {
               const netPerformancePercentWithCurrencyEffect = ctx.raw._data.netPerformancePercentWithCurrencyEffect;
               return [
-                ctx.raw._data.symbol, 
-                `${netPerformancePercentWithCurrencyEffect > 0 ? '+' : ''}${(ctx.raw._data.netPerformancePercentWithCurrencyEffect * 100).toFixed(2)}%`
+                `${(netPerformancePercentWithCurrencyEffect > 0 ? '+' : '')}${(ctx.raw._data.netPerformancePercentWithCurrencyEffect * 100).toFixed(2)}%`
               ];
             },
             position: 'top'
-          },
+          },          
           spacing: 1,
           tree: this.holdings
         }
