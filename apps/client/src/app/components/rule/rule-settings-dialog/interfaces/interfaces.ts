f8da265f5f4d8d34a3e11232a31b6c5d2ccad44a
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2024-10-12 21:04:09 +0200

    Feature/restructure XRayRulesSettings (#3898)
    * Restructure XRayRulesSettings
    
    * Update changelog

--- apps/client/src/app/components/rule/rule-settings-dialog/interfaces/interfaces.ts
+++ apps/client/src/app/components/rule/rule-settings-dialog/interfaces/interfaces.ts
@@ -1,5 +1,7 @@
 import { PortfolioReportRule } from '@ghostfolio/common/interfaces';
+import { XRayRulesSettings } from '@ghostfolio/common/types';
 
 export interface IRuleSettingsDialogParams {
   rule: PortfolioReportRule;
+  settings: XRayRulesSettings['AccountClusterRiskCurrentInvestment'];
 }
