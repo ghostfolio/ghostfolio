39d6d3900aa073bc990c1f2efc6d47599908b591
Dan <dan@ddev.ch>
2024-10-30 11:14:55 +0100

    Fix empty Arrow functions

--- apps/client/src/app/components/admin-market-data/asset-profile-dialog/asset-profile-dialog.component.ts
+++ apps/client/src/app/components/admin-market-data/asset-profile-dialog/asset-profile-dialog.component.ts
@@ -190,14 +190,14 @@ export class AssetProfileDialog implements OnDestroy, OnInit {
     this.adminService
       .gatherProfileDataBySymbol({ dataSource, symbol })
       .pipe(takeUntil(this.unsubscribeSubject))
-      .subscribe(() => {});
+      .subscribe();
   }
 
   public onGatherSymbol({ dataSource, symbol }: AssetProfileIdentifier) {
     this.adminService
       .gatherSymbol({ dataSource, symbol })
       .pipe(takeUntil(this.unsubscribeSubject))
-      .subscribe(() => {});
+      .subscribe();
   }
 
   public onGatherSymbolMissingOnly({
@@ -207,7 +207,7 @@ export class AssetProfileDialog implements OnDestroy, OnInit {
     this.adminService
       .gatherSymbolMissingOnly({ dataSource, symbol })
       .pipe(takeUntil(this.unsubscribeSubject))
-      .subscribe(() => {});
+      .subscribe();
   }
 
   public onImportHistoricalData() {
