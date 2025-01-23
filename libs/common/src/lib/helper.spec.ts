1cea7abee133a5cc30dd180247ac19cfef2475ff
Guillermo Fernandez <folez97@gmail.com>
2025-01-23 22:20:33 +0100

    Added more common languages and mocked navigator.language for completeness

--- libs/common/src/lib/helper.spec.ts
+++ libs/common/src/lib/helper.spec.ts
@@ -41,12 +41,71 @@ describe('Helper', () => {
   });
 
   describe('Get Number Format Group', () => {
+    let languageGetter;
+    beforeEach(() => {
+      languageGetter = jest.spyOn(window.navigator, 'language', 'get');
+    });
     it('Get en-US number format group', () => {
       expect(getNumberFormatGroup('en-US')).toEqual(',');
     });
 
+    it('Get en-US number format group when it is default', () => {
+      languageGetter.mockReturnValue('en-US');
+      expect(getNumberFormatGroup()).toEqual(',');
+    });
+
+    it('Get en-GB number format group', () => {
+      expect(getNumberFormatGroup('en-GB')).toEqual(',');
+    });
+
+    it('Get en-GB number format group when it is default', () => {
+      languageGetter.mockReturnValue('en-GB');
+      expect(getNumberFormatGroup()).toEqual(',');
+    });
+
     it('Get es-ES number format group', () => {
       expect(getNumberFormatGroup('es-ES')).toEqual('.');
+    });
+
+    it('Get es-ES number format group when it is default', () => {
+      languageGetter.mockReturnValue('es-ES');
+      expect(getNumberFormatGroup()).toEqual('.');
+    });
+
+    it('Get de-DE number format group', () => {
+      expect(getNumberFormatGroup('de-DE')).toEqual('.');
+    });
+
+    it('Get de-DE number format group when it is default', () => {
+      languageGetter.mockReturnValue('de-DE');
+      expect(getNumberFormatGroup()).toEqual('.');
+    });
+
+    it('Get de-CH number format group', () => {
+      expect(getNumberFormatGroup('de-CH')).toEqual('’');
+    });
+
+    it('Get de-CH number format group when it is default', () => {
+      languageGetter.mockReturnValue('de-CH');
+      expect(getNumberFormatGroup()).toEqual('’');
+    });
+
+    it('Get zh-CN number format group', () => {
+      expect(getNumberFormatGroup('zh-CN')).toEqual(',');
+    });
+
+    it('Get zh-CN number format group when it is default', () => {
+      languageGetter.mockReturnValue('zh-CN');
+      expect(getNumberFormatGroup()).toEqual(',');
+    });
+
+    it('Get ru-RU number format group', () => {
+      expect(getNumberFormatGroup('ru-RU')).toEqual(' ');
+    });
+
+    it('Get ru-RU number format group when it is default', () => {
+      languageGetter.mockReturnValue('ru-RU');
+      expect(getNumberFormatGroup()).toEqual(' ');
     });
   });
 });
