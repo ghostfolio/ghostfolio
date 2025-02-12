e54d1abce6cfd4169a8918bfe541ed7c64463a10
Shaunak Das <51281688+shaun-ak@users.noreply.github.com>
2025-02-12 09:13:58 +0530

    add asia pacific cluster risk to portfolioservice

--- apps/api/src/app/portfolio/portfolio.service.ts
+++ apps/api/src/app/portfolio/portfolio.service.ts
@@ -15,6 +15,7 @@ import { EconomicMarketClusterRiskDevelopedMarkets } from '@ghostfolio/api/model
 import { EconomicMarketClusterRiskEmergingMarkets } from '@ghostfolio/api/models/rules/economic-market-cluster-risk/emerging-markets';
 import { EmergencyFundSetup } from '@ghostfolio/api/models/rules/emergency-fund/emergency-fund-setup';
 import { FeeRatioInitialInvestment } from '@ghostfolio/api/models/rules/fees/fee-ratio-initial-investment';
+import { RegionalMarketClusterRiskAsiaPacific } from '@ghostfolio/api/models/rules/regional-market-cluster-risk/asia-pacific';
 import { RegionalMarketClusterRiskEmergingMarkets } from '@ghostfolio/api/models/rules/regional-market-cluster-risk/emerging-markets';
 import { RegionalMarketClusterRiskEurope } from '@ghostfolio/api/models/rules/regional-market-cluster-risk/europe';
 import { RegionalMarketClusterRiskNorthAmerica } from '@ghostfolio/api/models/rules/regional-market-cluster-risk/north-america';
@@ -1280,6 +1281,11 @@ export class PortfolioService {
         summary.ordersCount > 0
           ? await this.rulesService.evaluate(
               [
+                new RegionalMarketClusterRiskAsiaPacific(
+                  this.exchangeRateDataService,
+                  marketsAdvancedTotalInBaseCurrency,
+                  marketsAdvanced.asiaPacific.valueInBaseCurrency
+                ),
                 new RegionalMarketClusterRiskEmergingMarkets(
                   this.exchangeRateDataService,
                   marketsAdvancedTotalInBaseCurrency,
