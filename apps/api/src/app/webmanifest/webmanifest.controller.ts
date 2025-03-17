44e76bd80284688ff58b4bb62b77905193608483
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2025-03-17 22:07:21 +0100

    Refactoring

--- apps/api/src/app/webmanifest/webmanifest.controller.ts
+++ apps/api/src/app/webmanifest/webmanifest.controller.ts
@@ -1,31 +1,44 @@
-import { Controller, Get, Param, Res, Version, VERSION_NEUTRAL } from '@nestjs/common';
-import { Response } from 'express';
-import { interpolate } from '@ghostfolio/common/helper';
 import { ConfigurationService } from '@ghostfolio/api/services/configuration/configuration.service';
-import * as fs from 'fs';
-import * as path from 'path';
+import { interpolate } from '@ghostfolio/common/helper';
+
+import {
+  Controller,
+  Get,
+  Param,
+  Res,
+  Version,
+  VERSION_NEUTRAL
+} from '@nestjs/common';
+import { Response } from 'express';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 @Controller('assets')
 export class WebManifestController {
-  public webManifestTemplate: string;
+  private webManifest = '';
 
-  public constructor(public readonly configService: ConfigurationService) {
+  public constructor(
+    public readonly configurationService: ConfigurationService
+  ) {
     try {
-      this.webManifestTemplate = fs.readFileSync(
-        path.resolve(__dirname, '../../assets/site.webmanifest'), 
+      this.webManifest = readFileSync(
+        join(__dirname, 'assets', 'site.webmanifest'),
         'utf8'
       );
-    } catch (error) {
-      console.error('Error reading site.webmanifest:', error);
-      this.webManifestTemplate = ''; 
-    }
+    } catch {}
   }
 
   @Get('/:languageCode/site.webmanifest')
   @Version(VERSION_NEUTRAL)
-  public getWebManifest(@Param('languageCode') languageCode: string, @Res() res: Response): void {
-    const rootUrl = this.configService.get('ROOT_URL') || 'https://default.url.com';
-    const webManifest = interpolate(this.webManifestTemplate, { languageCode, rootUrl });
+  public getWebManifest(
+    @Param('languageCode') languageCode: string,
+    @Res() res: Response
+  ): void {
+    const rootUrl = this.configurationService.get('ROOT_URL');
+    const webManifest = interpolate(this.webManifest, {
+      languageCode,
+      rootUrl
+    });
 
     res.setHeader('Content-Type', 'application/json');
     res.send(webManifest);
