5d79f7c9cf83417e93d474868035701bfe081c4d
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2025-03-27 10:06:28 +0100

    Clean up

--- apps/api/src/services/symbol-profile/symbol-profile.service.ts
+++ apps/api/src/services/symbol-profile/symbol-profile.service.ts
@@ -126,40 +126,6 @@ export class SymbolProfileService {
       });
   }
 
-  public async getSymbolProfilesByUserSubscription({
-    withUserSubscription = false
-  }: {
-    withUserSubscription?: boolean;
-  }) {
-    return this.prismaService.symbolProfile.findMany({
-      include: {
-        Order: {
-          include: {
-            User: true
-          }
-        }
-      },
-      orderBy: [{ symbol: 'asc' }],
-      where: {
-        Order: withUserSubscription
-          ? {
-              some: {
-                User: {
-                  Subscription: { some: { expiresAt: { gt: new Date() } } }
-                }
-              }
-            }
-          : {
-              every: {
-                User: {
-                  Subscription: { none: { expiresAt: { gt: new Date() } } }
-                }
-              }
-            }
-      }
-    });
-  }
-
   public updateAssetProfileIdentifier(
     oldAssetProfileIdentifier: AssetProfileIdentifier,
     newAssetProfileIdentifier: AssetProfileIdentifier
