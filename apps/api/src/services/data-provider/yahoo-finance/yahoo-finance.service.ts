71ce80a202b8c99435a07e645f7aefb618b35646
Anatoly Popov <me@aensidhe.ru>
2025-05-02 13:50:09 +0300

    fix: don't fail whole request if one of providers fails, but log it
    Signed-off-by: Anatoly Popov <me@aensidhe.ru>

--- apps/api/src/services/data-provider/yahoo-finance/yahoo-finance.service.ts
+++ apps/api/src/services/data-provider/yahoo-finance/yahoo-finance.service.ts
@@ -315,6 +315,7 @@ export class YahooFinanceService implements DataProviderInterface {
       }
     } catch (error) {
       Logger.error(error, 'YahooFinanceService');
+      return { items: [] };
     }
 
     return { items };
