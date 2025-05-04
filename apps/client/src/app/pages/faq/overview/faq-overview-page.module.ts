ecffb53f07ecc3feed23be2e31be63c061fb6f4c
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2025-05-04 15:54:54 +0200

    Feature/extend faq pages (#4655)
    * Extend FAQ pages
    
    * Update changelog

--- apps/client/src/app/pages/faq/overview/faq-overview-page.module.ts
+++ apps/client/src/app/pages/faq/overview/faq-overview-page.module.ts
@@ -1,3 +1,5 @@
+import { GfPremiumIndicatorComponent } from '@ghostfolio/ui/premium-indicator';
+
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
@@ -7,7 +9,12 @@ import { FaqOverviewPageComponent } from './faq-overview-page.component';
 
 @NgModule({
   declarations: [FaqOverviewPageComponent],
-  imports: [CommonModule, FaqOverviewPageRoutingModule, MatCardModule],
+  imports: [
+    CommonModule,
+    FaqOverviewPageRoutingModule,
+    GfPremiumIndicatorComponent,
+    MatCardModule
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class FaqOverviewPageModule {}
