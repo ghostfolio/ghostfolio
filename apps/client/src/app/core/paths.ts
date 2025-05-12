9c2a7562bc27137920e7143ca92121b7fe1dca89
KenTandrian <kenricktan11@gmail.com>
2025-05-12 14:31:17 +0700

    feat(client): add snake-case placeholder

--- apps/client/src/app/core/paths.ts
+++ apps/client/src/app/core/paths.ts
@@ -1,12 +1,12 @@
 export const paths = {
-  about: $localize`about`,
-  faq: $localize`faq`,
-  features: $localize`features`,
-  license: $localize`license`,
-  markets: $localize`markets`,
-  pricing: $localize`pricing`,
-  privacyPolicy: $localize`privacy-policy`,
-  register: $localize`register`,
-  resources: $localize`resources`,
-  termsOfService: $localize`terms-of-service`
+  about: $localize`:snake-case:about`,
+  faq: $localize`:snake-case:faq`,
+  features: $localize`:snake-case:features`,
+  license: $localize`:snake-case:license`,
+  markets: $localize`:snake-case:markets`,
+  pricing: $localize`:snake-case:pricing`,
+  privacyPolicy: $localize`:snake-case:privacy-policy`,
+  register: $localize`:snake-case:register`,
+  resources: $localize`:snake-case:resources`,
+  termsOfService: $localize`:snake-case:terms-of-service`
 };
