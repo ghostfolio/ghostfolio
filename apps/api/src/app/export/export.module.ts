74c46e2f2a0b645ba8cc830141fb3197976c3fe1
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2025-06-03 20:08:17 +0200

    Respect filter by holding when exporting activities

--- apps/api/src/app/export/export.module.ts
+++ apps/api/src/app/export/export.module.ts
@@ -1,5 +1,6 @@
 import { AccountModule } from '@ghostfolio/api/app/account/account.module';
 import { OrderModule } from '@ghostfolio/api/app/order/order.module';
+import { TransformDataSourceInRequestModule } from '@ghostfolio/api/interceptors/transform-data-source-in-request/transform-data-source-in-request.module';
 import { ApiModule } from '@ghostfolio/api/services/api/api.module';
 import { TagModule } from '@ghostfolio/api/services/tag/tag.module';
 
@@ -9,8 +10,15 @@ import { ExportController } from './export.controller';
 import { ExportService } from './export.service';
 
 @Module({
-  imports: [AccountModule, ApiModule, OrderModule, TagModule],
   controllers: [ExportController],
+  imports: [
+    AccountModule,
+    ApiModule,
+    OrderModule,
+    TagModule,
+    TransformDataSourceInRequestModule
+  ],
+
   providers: [ExportService]
 })
 export class ExportModule {}
