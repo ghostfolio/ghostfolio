f0a8896d7dc27d634bad3c3730a5415bb318ddfb
csehatt741 <att741@gmail.com>
2025-06-30 16:16:32 +0200

    MatButtonModule, MatCardModule added to OpenSourceSoftwareFriendsPageComponent

--- apps/client/src/app/pages/about/oss-friends/oss-friends-page.component.ts
+++ apps/client/src/app/pages/about/oss-friends/oss-friends-page.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnDestroy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { Subject } from 'rxjs';
 
 const ossFriends = require('../../../../assets/oss-friends.json');
 
 @Component({
+  imports: [MatButtonModule, MatCardModule],
   selector: 'gf-oss-friends-page',
   styleUrls: ['./oss-friends-page.scss'],
   templateUrl: './oss-friends-page.html'
