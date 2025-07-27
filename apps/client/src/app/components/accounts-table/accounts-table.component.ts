d68048ae03301e4f0629784b698d58a2a6e0b096
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2025-07-27 17:01:25 +0200

    Clean up

--- apps/client/src/app/components/accounts-table/accounts-table.component.ts
+++ apps/client/src/app/components/accounts-table/accounts-table.component.ts
@@ -60,7 +60,7 @@ export class GfAccountsTableComponent implements OnChanges, OnDestroy {
   @Input() hasPermissionToOpenDetails = true;
   @Input() locale = getLocale();
   @Input() showActions: boolean;
-  @Input() showAllocationInPercentage = false;
+  @Input() showAllocationInPercentage: boolean;
   @Input() showBalance = true;
   @Input() showFooter = true;
   @Input() showTransactions = true;
