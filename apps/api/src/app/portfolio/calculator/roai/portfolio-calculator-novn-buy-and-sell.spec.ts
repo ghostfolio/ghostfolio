84a130169250019e36231db6250889aa22454375
Attila Cseh <att741@gmail.com>
2025-07-30 11:15:16 +0200

    tests fixed

--- apps/api/src/app/portfolio/calculator/roai/portfolio-calculator-novn-buy-and-sell.spec.ts
+++ apps/api/src/app/portfolio/calculator/roai/portfolio-calculator-novn-buy-and-sell.spec.ts
@@ -18,6 +18,7 @@ import { PortfolioSnapshotServiceMock } from '@ghostfolio/api/services/queues/po
 import { parseDate } from '@ghostfolio/common/helper';
 import { PerformanceCalculationType } from '@ghostfolio/common/types/performance-calculation-type.type';
 
+import { Tag } from '@prisma/client';
 import { Big } from 'big.js';
 import { join } from 'path';
 
@@ -111,6 +112,9 @@ describe('PortfolioCalculator', () => {
           name: 'Novartis AG',
           symbol: activity.symbol
         },
+        tags: activity.tags?.map((id) => {
+          return { id } as Tag;
+        }),
         unitPriceInAssetProfileCurrency: activity.unitPrice
       }));
 
