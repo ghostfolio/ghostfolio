0a79a27e0db729383aaec369b7bab626f36da803
SK Akram <skcodewizard786@gmail.com>
2025-10-01 18:03:36 +0530

    Task/prefix home watchlist component with Gf (#5640)
    * Prefix home watchlist component with Gf

--- apps/client/src/app/pages/home/home-page.routes.ts
+++ apps/client/src/app/pages/home/home-page.routes.ts
@@ -2,7 +2,7 @@ import { GfHomeHoldingsComponent } from '@ghostfolio/client/components/home-hold
 import { HomeMarketComponent } from '@ghostfolio/client/components/home-market/home-market.component';
 import { GfHomeOverviewComponent } from '@ghostfolio/client/components/home-overview/home-overview.component';
 import { GfHomeSummaryComponent } from '@ghostfolio/client/components/home-summary/home-summary.component';
-import { HomeWatchlistComponent } from '@ghostfolio/client/components/home-watchlist/home-watchlist.component';
+import { GfHomeWatchlistComponent } from '@ghostfolio/client/components/home-watchlist/home-watchlist.component';
 import { MarketsComponent } from '@ghostfolio/client/components/markets/markets.component';
 import { AuthGuard } from '@ghostfolio/client/core/auth.guard';
 import { internalRoutes } from '@ghostfolio/common/routes/routes';
@@ -41,7 +41,7 @@ export const routes: Routes = [
       },
       {
         path: internalRoutes.home.subRoutes.watchlist.path,
-        component: HomeWatchlistComponent,
+        component: GfHomeWatchlistComponent,
         title: internalRoutes.home.subRoutes.watchlist.title
       }
     ],
