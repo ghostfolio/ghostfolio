f4a1f43d44dcda25191445ce57d7013cb1c31130
Dibyendu Sahoo <dibyendusahoo03@gmail.com>
2025-10-21 00:07:35 +0530

    Task/extend rule settings interface by locale (#5802)
    * Extend rule settings interface by locale

--- apps/api/src/models/rules/account-cluster-risk/current-investment.ts
+++ apps/api/src/models/rules/account-cluster-risk/current-investment.ts
@@ -121,9 +121,14 @@ export class AccountClusterRiskCurrentInvestment extends Rule<Settings> {
     });
   }
 
-  public getSettings({ baseCurrency, xRayRules }: UserSettings): Settings {
+  public getSettings({
+    baseCurrency,
+    locale,
+    xRayRules
+  }: UserSettings): Settings {
     return {
       baseCurrency,
+      locale,
       isActive: xRayRules?.[this.getKey()]?.isActive ?? true,
       thresholdMax: xRayRules?.[this.getKey()]?.thresholdMax ?? 0.5
     };
