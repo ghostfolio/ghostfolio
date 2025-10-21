3fec3514c1a786cd1c014de54de9b19d21ae2f12
Harsh Santwani <96873014+HydrallHarsh@users.noreply.github.com>
2025-10-21 19:12:30 +0530

    Task/improve typings of getOrderById() functionality (#5810)
    * Improve typings of getOrderById() functionality

--- apps/client/src/app/services/data.service.ts
+++ apps/client/src/app/services/data.service.ts
@@ -9,10 +9,7 @@ import { CreateTagDto } from '@ghostfolio/api/app/endpoints/tags/create-tag.dto'
 import { UpdateTagDto } from '@ghostfolio/api/app/endpoints/tags/update-tag.dto';
 import { CreateWatchlistItemDto } from '@ghostfolio/api/app/endpoints/watchlist/create-watchlist-item.dto';
 import { CreateOrderDto } from '@ghostfolio/api/app/order/create-order.dto';
-import {
-  Activities,
-  Activity
-} from '@ghostfolio/api/app/order/interfaces/activities.interface';
+import { Activities } from '@ghostfolio/api/app/order/interfaces/activities.interface';
 import { UpdateOrderDto } from '@ghostfolio/api/app/order/update-order.dto';
 import { SymbolItem } from '@ghostfolio/api/app/symbol/interfaces/symbol-item.interface';
 import { DeleteOwnUserDto } from '@ghostfolio/api/app/user/delete-own-user.dto';
@@ -27,6 +24,7 @@ import {
   AccessTokenResponse,
   AccountBalancesResponse,
   AccountsResponse,
+  ActivityResponse,
   AiPromptResponse,
   ApiKeyResponse,
   AssetProfileIdentifier,
@@ -248,7 +246,7 @@ export class DataService {
   }
 
   public fetchActivity(aActivityId: string) {
-    return this.http.get<Activity>(`/api/v1/order/${aActivityId}`).pipe(
+    return this.http.get<ActivityResponse>(`/api/v1/order/${aActivityId}`).pipe(
       map((activity) => {
         activity.createdAt = parseISO(activity.createdAt as unknown as string);
         activity.date = parseISO(activity.date as unknown as string);
