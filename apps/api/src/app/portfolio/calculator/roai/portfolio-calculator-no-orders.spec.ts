f19d9c78aa8e96a1e3320ed7f751ef5f35c07b0d
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2025-10-22 07:37:15 +0200

    Task/clean up unused eslint-disable-next-line directives (#5782)
    * Clean up unused eslint-disable-next-line directives

--- apps/api/src/app/portfolio/calculator/roai/portfolio-calculator-no-orders.spec.ts
+++ apps/api/src/app/portfolio/calculator/roai/portfolio-calculator-no-orders.spec.ts
@@ -15,7 +15,6 @@ import { Big } from 'big.js';
 
 jest.mock('@ghostfolio/api/app/portfolio/current-rate.service', () => {
   return {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
     CurrentRateService: jest.fn().mockImplementation(() => {
       return CurrentRateServiceMock;
     })
@@ -26,7 +25,6 @@ jest.mock(
   '@ghostfolio/api/services/queues/portfolio-snapshot/portfolio-snapshot.service',
   () => {
     return {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
       PortfolioSnapshotService: jest.fn().mockImplementation(() => {
         return PortfolioSnapshotServiceMock;
       })
@@ -36,7 +34,6 @@ jest.mock(
 
 jest.mock('@ghostfolio/api/app/redis-cache/redis-cache.service', () => {
   return {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
     RedisCacheService: jest.fn().mockImplementation(() => {
       return RedisCacheServiceMock;
     })
