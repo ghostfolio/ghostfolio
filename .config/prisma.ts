e891299317bb0f83fb05d19bcc5180880499662d
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2025-10-22 19:05:45 +0200

    Revert paths

--- .config/prisma.ts
+++ .config/prisma.ts
@@ -7,8 +7,8 @@ expand(config({ quiet: true }));
 
 export default defineConfig({
   migrations: {
-    path: join('..', 'prisma', 'migrations'),
-    seed: `node ${join('..', 'prisma', 'seed.mts')}`
+    path: join('prisma', 'migrations'),
+    seed: `node ${join('prisma', 'seed.mts')}`
   },
-  schema: join('..', 'prisma', 'schema.prisma')
+  schema: join('prisma', 'schema.prisma')
 });
