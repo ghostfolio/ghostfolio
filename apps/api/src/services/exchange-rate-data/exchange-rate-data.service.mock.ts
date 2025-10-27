0a677b5c768447080994ac4af2375bdceb1662de
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2025-10-27 21:12:28 +0100

    Set up test

--- apps/api/src/services/exchange-rate-data/exchange-rate-data.service.mock.ts
+++ apps/api/src/services/exchange-rate-data/exchange-rate-data.service.mock.ts
@@ -19,14 +19,19 @@ export const ExchangeRateDataServiceMock = {
       });
     } else if (targetCurrency === 'EUR') {
       return Promise.resolve({
-        EUREUR: {},
-        USDEUR: {}
+        EUREUR: {
+          '2021-12-12': 1
+        },
+        USDEUR: {
+          '2021-12-12': 0.8855
+        }
       });
     } else if (targetCurrency === 'USD') {
       return Promise.resolve({
         USDUSD: {
           '2018-01-01': 1,
           '2021-11-16': 1,
+          '2021-12-12': 1,
           '2023-07-10': 1
         }
       });
