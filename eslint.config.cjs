7331d530a8442bcf8403fd053c1003c5682ac3b9
KenTandrian <kenricktan11@gmail.com>
2025-11-01 22:10:24 +0700

    fix(lint): remove @typescript-eslint/no-unsafe-function-type override

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -170,7 +170,6 @@ module.exports = [
         '@typescript-eslint/no-unsafe-argument': 'warn',
         '@typescript-eslint/no-unsafe-assignment': 'warn',
         '@typescript-eslint/no-unsafe-enum-comparison': 'warn',
-        '@typescript-eslint/no-unsafe-function-type': 'warn',
         '@typescript-eslint/no-unsafe-member-access': 'warn',
         '@typescript-eslint/no-unsafe-return': 'warn',
         '@typescript-eslint/no-unsafe-call': 'warn',
