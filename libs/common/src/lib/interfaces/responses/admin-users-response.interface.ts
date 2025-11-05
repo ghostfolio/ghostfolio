e3f25eb9121d94e6f56b8d7258b9a065e46d41c6
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2025-11-05 20:51:54 +0100

    Refactoring

--- libs/common/src/lib/interfaces/responses/admin-users-response.interface.ts
+++ libs/common/src/lib/interfaces/responses/admin-users-response.interface.ts
@@ -1,18 +1,4 @@
-import { Role } from '@prisma/client';
-
-interface AdminUser {
-  accountCount: number;
-  activityCount: number;
-  country: string;
-  createdAt: Date;
-  dailyApiRequests: number;
-  engagement: number;
-  id: string;
-  lastActivity: Date;
-  role: Role;
-}
-
-export interface AdminUserResponse extends AdminUser {}
+import { AdminUser } from '../admin-user.interface';
 
 export interface AdminUsersResponse {
   count: number;
