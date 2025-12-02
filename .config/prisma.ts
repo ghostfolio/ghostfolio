b64e119846c669e7eba8975576d49945c76530fd
KenTandrian <kenricktan11@gmail.com>
2025-12-03 00:44:48 +0700

    feat(db): move url to prisma config

--- .config/prisma.ts
+++ .config/prisma.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@prisma/config';
+import { defineConfig, env } from '@prisma/config';
 import { config } from 'dotenv';
 import { expand } from 'dotenv-expand';
 import { join } from 'node:path';
@@ -6,6 +6,9 @@ import { join } from 'node:path';
 expand(config({ quiet: true }));
 
 export default defineConfig({
+  datasource: {
+    url: env('DATABASE_URL')
+  },
   migrations: {
     path: join(__dirname, '..', 'prisma', 'migrations'),
     seed: `node ${join(__dirname, '..', 'prisma', 'seed.mts')}`
