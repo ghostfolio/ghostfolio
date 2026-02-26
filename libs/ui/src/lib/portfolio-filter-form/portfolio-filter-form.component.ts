ab9c518cfe40bd66faf4bc282203b9d52a32f92b
Kenrick Tandrian <60643640+KenTandrian@users.noreply.github.com>
2026-02-26 19:07:44 +0000

    feat(lib): change holdings to input signal

--- libs/ui/src/lib/portfolio-filter-form/portfolio-filter-form.component.ts
+++ libs/ui/src/lib/portfolio-filter-form/portfolio-filter-form.component.ts
@@ -57,12 +57,12 @@ import { PortfolioFilterFormValue } from './interfaces';
 export class GfPortfolioFilterFormComponent
   implements ControlValueAccessor, OnInit, OnChanges
 {
-  @Input() holdings: PortfolioPosition[] = [];
   @Input() tags: Filter[] = [];
   @Input() disabled = false;
 
   public readonly accounts = input<AccountWithPlatform[]>([]);
   public readonly assetClasses = input<Filter[]>([]);
+  public readonly holdings = input<PortfolioPosition[]>([]);
 
   public filterForm: FormGroup<{
     account: FormControl<string | null>;
