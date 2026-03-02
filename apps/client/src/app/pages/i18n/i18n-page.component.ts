55952d3cc81eea937265b492c120515320b50096
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-03-02 19:30:50 +0100

    Task/remove unused OnDestroy hook in i18n page component (#6441)
    * Remove unused OnDestroy hook

--- apps/client/src/app/pages/i18n/i18n-page.component.ts
+++ apps/client/src/app/pages/i18n/i18n-page.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Subject } from 'rxjs';
 
 @Component({
   host: { class: 'page' },
@@ -8,11 +7,4 @@ import { Subject } from 'rxjs';
   styleUrls: ['./i18n-page.scss'],
   templateUrl: './i18n-page.html'
 })
-export class GfI18nPageComponent {
-  private unsubscribeSubject = new Subject<void>();
-
-  public ngOnDestroy() {
-    this.unsubscribeSubject.next();
-    this.unsubscribeSubject.complete();
-  }
-}
+export class GfI18nPageComponent {}
