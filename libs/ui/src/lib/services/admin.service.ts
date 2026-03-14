da2901d0b3189e9792497f1684c44a9134db4e81
KenTandrian <kenricktan11@gmail.com>
2026-03-14 23:38:33 +0700

    feat(lib): add isNumber check

--- libs/ui/src/lib/services/admin.service.ts
+++ libs/ui/src/lib/services/admin.service.ts
@@ -29,6 +29,7 @@ import { Injectable, inject } from '@angular/core';
 import { SortDirection } from '@angular/material/sort';
 import { DataSource, MarketData, Platform } from '@prisma/client';
 import { JobStatus } from 'bull';
+import { isNumber } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
@@ -156,11 +157,13 @@ export class AdminService {
   }) {
     let params = new HttpParams();
 
-    if (skip) {
+    if (isNumber(skip)) {
       params = params.append('skip', skip);
     }
 
-    params = params.append('take', take);
+    if (isNumber(take)) {
+      params = params.append('take', take);
+    }
 
     return this.http.get<AdminUsersResponse>('/api/v1/admin/user', { params });
   }
