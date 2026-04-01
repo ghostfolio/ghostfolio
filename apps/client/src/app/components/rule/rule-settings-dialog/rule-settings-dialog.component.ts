78f140b16caf4af3461dae764f698695a8902a00
sshiao <sshiao@users.noreply.github.com>
2026-03-31 17:15:48 -0700

    fix: disable Save button until form is dirty in rule settings dialog (Fixes #6479)

--- apps/client/src/app/components/rule/rule-settings-dialog/rule-settings-dialog.component.ts
+++ apps/client/src/app/components/rule/rule-settings-dialog/rule-settings-dialog.component.ts
@@ -1,9 +1,8 @@
-import { XRayRulesSettings } from '@ghostfolio/common/interfaces';
 import { GfValueComponent } from '@ghostfolio/ui/value';
 
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
   MAT_DIALOG_DATA,
@@ -17,21 +16,33 @@ import { RuleSettingsDialogParams } from './interfaces/interfaces';
 @Component({
   imports: [
     CommonModule,
-    FormsModule,
     GfValueComponent,
     MatButtonModule,
     MatDialogModule,
-    MatSliderModule
+    MatSliderModule,
+    ReactiveFormsModule
   ],
   selector: 'gf-rule-settings-dialog',
   styleUrls: ['./rule-settings-dialog.scss'],
   templateUrl: './rule-settings-dialog.html'
 })
 export class GfRuleSettingsDialogComponent {
-  public settings: XRayRulesSettings['AccountClusterRiskCurrentInvestment'];
+  public thresholdMaxFormControl = new FormControl<number>(
+    this.data.settings.thresholdMax
+  );
+  public thresholdMinFormControl = new FormControl<number>(
+    this.data.settings.thresholdMin
+  );
 
   public constructor(
     @Inject(MAT_DIALOG_DATA) public data: RuleSettingsDialogParams,
     public dialogRef: MatDialogRef<GfRuleSettingsDialogComponent>
   ) {}
+
+  public onSave() {
+    this.dialogRef.close({
+      thresholdMax: this.thresholdMaxFormControl.value,
+      thresholdMin: this.thresholdMinFormControl.value
+    });
+  }
 }
