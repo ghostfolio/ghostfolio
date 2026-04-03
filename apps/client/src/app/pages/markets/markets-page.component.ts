3ad4428604e395a4d2b480c5763f675e044f8df5
Trevin Chow <trevin@trevinchow.com>
2026-04-02 23:49:14 -0700

    Feature/eliminate OnDestroy lifecycle hook from markets page component (#6653)
    Eliminate OnDestroy lifecycle hook

--- apps/client/src/app/pages/markets/markets-page.component.ts
+++ apps/client/src/app/pages/markets/markets-page.component.ts
@@ -1,7 +1,6 @@
 import { GfHomeMarketComponent } from '@ghostfolio/client/components/home-market/home-market.component';
 
-import { Component, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   host: { class: 'page' },
@@ -10,11 +9,4 @@ import { Subject } from 'rxjs';
   styleUrls: ['./markets-page.scss'],
   templateUrl: './markets-page.html'
 })
-export class GfMarketsPageComponent implements OnDestroy {
-  private unsubscribeSubject = new Subject<void>();
-
-  public ngOnDestroy() {
-    this.unsubscribeSubject.next();
-    this.unsubscribeSubject.complete();
-  }
-}
+export class GfMarketsPageComponent {}
