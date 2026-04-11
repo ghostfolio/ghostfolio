dc2a871f4e1bc823c711c35d2630ddd5f2b5ddd5
David Requeno <108202767+DavidReque@users.noreply.github.com>
2026-04-11 09:57:50 -0600

    Task/upgrade to Nx 22.6 (#6704)
    * Upgrade to Nx 22.6
    
    * Update changelog

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { DEFAULT_HOST, DEFAULT_PORT } from '@ghostfolio/common/config';
 export const environment = {
   production: true,
   rootUrl: `http://${DEFAULT_HOST}:${DEFAULT_PORT}`,
-  version: `${require('../../../../package.json').version}`
+  version: process.env.npm_package_version ?? 'dev'
 };
