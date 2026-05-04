f0ee1f38b6e850fc7e4a6e06dd948517f40337e3
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-05-04 17:06:47 +0200

    Add Revolut

--- apps/client/src/app/pages/pricing/pricing-page.component.ts
+++ apps/client/src/app/pages/pricing/pricing-page.component.ts
@@ -78,6 +78,7 @@ export class GfPricingPageComponent implements OnInit {
     'Interactive Brokers',
     'Mintos',
     'Monefit SmartSaver',
+    'Revolut',
     'Swissquote',
     'VIAC',
     'Zak'
