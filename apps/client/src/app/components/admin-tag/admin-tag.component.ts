f8fdcddc46faaa58dc88d4385dc7860dbc331a55
KenTandrian <kenricktan11@gmail.com>
2026-05-17 16:18:24 +0700

    feat(client): migrate constructor based DI to inject function

--- apps/client/src/app/components/admin-tag/admin-tag.component.ts
+++ apps/client/src/app/components/admin-tag/admin-tag.component.ts
@@ -11,6 +11,7 @@ import {
   ChangeDetectorRef,
   Component,
   DestroyRef,
+  inject,
   Input,
   OnInit,
   ViewChild
@@ -61,17 +62,17 @@ export class GfAdminTagComponent implements OnInit {
   public displayedColumns = ['name', 'userId', 'activities', 'actions'];
   public tags: Tag[];
 
-  public constructor(
-    private changeDetectorRef: ChangeDetectorRef,
-    private dataService: DataService,
-    private destroyRef: DestroyRef,
-    private deviceDetectorService: DeviceDetectorService,
-    private dialog: MatDialog,
-    private notificationService: NotificationService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private userService: UserService
-  ) {
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+  private readonly dataService = inject(DataService);
+  private readonly destroyRef = inject(DestroyRef);
+  private readonly deviceDetectorService = inject(DeviceDetectorService);
+  private readonly dialog = inject(MatDialog);
+  private readonly notificationService = inject(NotificationService);
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  private readonly userService = inject(UserService);
+
+  public constructor() {
     this.route.queryParams
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe((params) => {
