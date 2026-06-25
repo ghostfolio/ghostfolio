f7e90f3c1c8385b9c98d6eb28cf47b7ccea20ee0
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-06-25 09:19:16 +0200

    Improve data source encoding

--- apps/api/src/helper/data-source.helper.ts
+++ apps/api/src/helper/data-source.helper.ts
@@ -1,4 +1,21 @@
 import { DataSource } from '@prisma/client';
+import { createHash } from 'node:crypto';
+
+const decodedDataSourceMap: Record<string, DataSource> = Object.values(
+  DataSource
+).reduce((map, dataSource) => {
+  map[encodeDataSource(dataSource)] = dataSource;
+
+  return map;
+}, {});
+
+export function encodeDataSource(aDataSource: DataSource) {
+  if (!aDataSource) {
+    return undefined;
+  }
+
+  return createHash('sha256').update(aDataSource).digest('hex').slice(0, 8);
+}
 
 export function getMaskedGhostfolioDataSource({
   dataSource,
@@ -10,4 +27,10 @@ export function getMaskedGhostfolioDataSource({
   return ghostfolioDataSources.includes(dataSource)
     ? DataSource.GHOSTFOLIO
     : dataSource;
+}
+
+export function decodeDataSource(encodedDataSource: string) {
+  return encodedDataSource
+    ? decodedDataSourceMap[encodedDataSource]
+    : undefined;
 }
