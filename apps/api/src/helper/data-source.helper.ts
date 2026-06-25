f86594fe7530f070ef9cf22e464a9f950136b25f
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-06-25 17:51:43 +0200

    Improve data source encoding

--- apps/api/src/helper/data-source.helper.ts
+++ apps/api/src/helper/data-source.helper.ts
@@ -1,20 +1,57 @@
 import { DataSource } from '@prisma/client';
 import { createHash } from 'node:crypto';
 
-const decodedDataSourceMap: Record<string, DataSource> = Object.values(
-  DataSource
-).reduce((map, dataSource) => {
-  map[encodeDataSource(dataSource)] = dataSource;
+const encodedDataSourceByDataSource = new Map<DataSource, string>(
+  Object.values(DataSource).map((dataSource) => {
+    return [dataSource, hashDataSource(dataSource)];
+  })
+);
 
-  return map;
-}, {});
+const dataSourceByEncodedDataSource = new Map<string, DataSource>(
+  [...encodedDataSourceByDataSource].map(([dataSource, encodedDataSource]) => {
+    return [encodedDataSource, dataSource];
+  })
+);
 
-export function encodeDataSource(aDataSource: DataSource) {
-  if (!aDataSource) {
+/**
+ * @deprecated Backward compatibility to support importing data that was
+ * exported using the previous data source encoding
+ */
+const dataSourceByDeprecatedEncodedDataSource = new Map<string, DataSource>(
+  Object.values(DataSource).map((dataSource) => {
+    return [deprecatedHashDataSource(dataSource), dataSource];
+  })
+);
+
+/**
+ * @deprecated Backward compatibility (see above)
+ */
+function deprecatedHashDataSource(dataSource: DataSource) {
+  return Buffer.from(dataSource, 'utf-8').toString('hex');
+}
+
+function hashDataSource(dataSource: DataSource) {
+  return createHash('sha256').update(dataSource).digest('hex').slice(0, 8);
+}
+
+export function decodeDataSource(encodedDataSource: string) {
+  if (!encodedDataSource) {
     return undefined;
   }
 
-  return createHash('sha256').update(aDataSource).digest('hex').slice(0, 8);
+  return (
+    dataSourceByEncodedDataSource.get(encodedDataSource) ??
+    dataSourceByDeprecatedEncodedDataSource.get(encodedDataSource) ??
+    encodedDataSource
+  );
+}
+
+export function encodeDataSource(dataSource: DataSource) {
+  if (!dataSource) {
+    return undefined;
+  }
+
+  return encodedDataSourceByDataSource.get(dataSource);
 }
 
 export function getMaskedGhostfolioDataSource({
@@ -27,10 +64,4 @@ export function getMaskedGhostfolioDataSource({
   return ghostfolioDataSources.includes(dataSource)
     ? DataSource.GHOSTFOLIO
     : dataSource;
-}
-
-export function decodeDataSource(encodedDataSource: string) {
-  return encodedDataSource
-    ? decodedDataSourceMap[encodedDataSource]
-    : undefined;
 }
