95a2e587ec475f019a7fd695fdf95f9a523bd53c
KenTandrian <kenricktan11@gmail.com>
2026-07-03 21:49:36 +0700

    feat(api): configure webpack plugin

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,6 +1,44 @@
-const { composePlugins, withNx } = require('@nx/webpack');
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 
-module.exports = composePlugins(withNx(), (config, { options, context }) => {
-  // Customize webpack config here
-  return config;
+// These options were migrated by @nx/webpack:convert-to-inferred from
+// the project.json file and merged with the options in this file
+const configValues = {
+  build: {
+    default: {
+      compiler: 'tsc',
+      deleteOutputPath: false,
+      main: './src/main.ts',
+      outputPath: 'dist/apps/api',
+      sourceMap: true,
+      target: 'node',
+      tsConfig: './tsconfig.app.json'
+    },
+    production: {
+      generatePackageJson: true,
+      optimization: true,
+      extractLicenses: true,
+      inspect: false,
+      fileReplacements: [
+        {
+          replace: './src/environments/environment.ts',
+          with: './src/environments/environment.prod.ts'
+        }
+      ]
+    }
+  }
+};
+
+// Determine the correct configValue to use based on the configuration
+const configuration = process.env.NX_TASK_TARGET_CONFIGURATION || 'default';
+
+const buildOptions = {
+  ...configValues.build.default,
+  ...configValues.build[configuration]
+};
+
+/**
+ * @type {import('webpack').WebpackOptionsNormalized}
+ */
+module.exports = async () => ({
+  plugins: [new NxAppWebpackPlugin(buildOptions)]
 });
