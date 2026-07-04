7f00c2881197ecc120cb2bb4517abea331a8f7c1
Kenrick Tandrian <60643640+KenTandrian@users.noreply.github.com>
2026-07-04 13:12:48 +0700

    Task/improve type safety in accounts page component (#7195)
    * feat(client): resolve type errors
    
    * feat(client): enforce encapsulation
    
    * feat(client): replace constructor based DI with inject functions
    
    * feat(client): replace deprecated getDeviceInfo

--- apps/client/src/app/pages/accounts/create-or-update-account-dialog/interfaces/interfaces.ts
+++ apps/client/src/app/pages/accounts/create-or-update-account-dialog/interfaces/interfaces.ts
@@ -1,5 +1,7 @@
 import { Account } from '@prisma/client';
 
 export interface CreateOrUpdateAccountDialogParams {
-  account: Omit<Account, 'createdAt' | 'updatedAt' | 'userId'>;
+  account: Omit<Account, 'createdAt' | 'id' | 'updatedAt' | 'userId'> & {
+    id: string | null;
+  };
 }
