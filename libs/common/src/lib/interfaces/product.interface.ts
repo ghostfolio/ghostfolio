f1f66663ee3e8c7fdcc9667f0e8a496c8ddcf7f4
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-07-05 10:34:01 +0200

    Task/setup categories and platforms for personal finance tools (#7237)
    Setup categories and platforms

--- libs/common/src/lib/interfaces/product.interface.ts
+++ libs/common/src/lib/interfaces/product.interface.ts
@@ -1,5 +1,8 @@
+import { ProductCategory, ProductPlatform } from '@ghostfolio/common/types';
+
 export interface Product {
   alias?: string;
+  categories?: ProductCategory[];
   founded?: number;
   hasFreePlan?: boolean;
   hasSelfHostingAbility?: boolean;
@@ -10,6 +13,7 @@ export interface Product {
   name: string;
   note?: string;
   origin?: string;
+  platforms?: ProductPlatform[];
   pricingPerYear?: string;
   regions?: string[];
   slogan?: string;
