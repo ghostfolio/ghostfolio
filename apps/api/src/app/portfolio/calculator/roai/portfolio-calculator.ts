3fdf9d7e3485b144caf30521ce88100047038afc
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-07-12 17:37:45 +0200

    Task/rename SymbolProfile to assetProfile in portfolio order interface (#7310)
    Rename SymbolProfile to assetProfile

--- apps/api/src/app/portfolio/calculator/roai/portfolio-calculator.ts
+++ apps/api/src/app/portfolio/calculator/roai/portfolio-calculator.ts
@@ -192,12 +192,12 @@ export class RoaiPortfolioCalculator extends PortfolioCalculator {
 
     // Clone orders to keep the original values in this.orders
     let orders: PortfolioOrderItem[] = cloneDeep(
-      this.activities.filter(({ SymbolProfile }) => {
-        return SymbolProfile.symbol === symbol;
+      this.activities.filter(({ assetProfile }) => {
+        return assetProfile.symbol === symbol;
       })
     );
 
-    const isCash = orders[0]?.SymbolProfile?.assetSubClass === 'CASH';
+    const isCash = orders[0]?.assetProfile?.assetSubClass === 'CASH';
 
     if (orders.length <= 0) {
       return {
@@ -299,32 +299,30 @@ export class RoaiPortfolioCalculator extends PortfolioCalculator {
       };
     }
 
+    const assetProfile: PortfolioOrderItem['assetProfile'] = {
+      dataSource,
+      symbol,
+      assetSubClass: isCash ? 'CASH' : undefined
+    };
+
     // Add a synthetic order at the start and the end date
     orders.push({
+      assetProfile,
       date: startDateString,
       fee: new Big(0),
       feeInBaseCurrency: new Big(0),
       itemType: 'start',
       quantity: new Big(0),
-      SymbolProfile: {
-        dataSource,
-        symbol,
-        assetSubClass: isCash ? 'CASH' : undefined
-      },
       type: 'BUY',
       unitPrice: unitPriceAtStartDate
     });
 
     orders.push({
+      assetProfile,
       date: endDateString,
       fee: new Big(0),
       feeInBaseCurrency: new Big(0),
       itemType: 'end',
-      SymbolProfile: {
-        dataSource,
-        symbol,
-        assetSubClass: isCash ? 'CASH' : undefined
-      },
       quantity: new Big(0),
       type: 'BUY',
       unitPrice: unitPriceAtEndDate
@@ -357,15 +355,11 @@ export class RoaiPortfolioCalculator extends PortfolioCalculator {
         }
       } else {
         orders.push({
+          assetProfile,
           date: dateString,
           fee: new Big(0),
           feeInBaseCurrency: new Big(0),
           quantity: new Big(0),
-          SymbolProfile: {
-            dataSource,
-            symbol,
-            assetSubClass: isCash ? 'CASH' : undefined
-          },
           type: 'BUY',
           unitPrice: marketSymbolMap[dateString]?.[symbol] ?? lastUnitPrice,
           unitPriceFromMarketData:
