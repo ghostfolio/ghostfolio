69254032a384655d2f8447674bc53a9508ed068e
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-07-13 21:53:48 +0200

    Feature/add markets endpoint for Fear & Greed index to Ghostfolio data provider (#7323)
    * Add markets endpoint
    
    * Update changelog

--- apps/api/src/app/endpoints/market-data/market-data.controller.ts
+++ apps/api/src/app/endpoints/market-data/market-data.controller.ts
@@ -1,14 +1,8 @@
 import { SymbolService } from '@ghostfolio/api/app/symbol/symbol.service';
 import { HasPermission } from '@ghostfolio/api/decorators/has-permission.decorator';
 import { HasPermissionGuard } from '@ghostfolio/api/guards/has-permission.guard';
-import { DataProviderService } from '@ghostfolio/api/services/data-provider/data-provider.service';
 import { MarketDataService } from '@ghostfolio/api/services/market-data/market-data.service';
 import { SymbolProfileService } from '@ghostfolio/api/services/symbol-profile/symbol-profile.service';
-import {
-  ghostfolioFearAndGreedIndexDataSourceCryptocurrencies,
-  ghostfolioFearAndGreedIndexSymbolCryptocurrencies,
-  ghostfolioFearAndGreedIndexSymbolStocks
-} from '@ghostfolio/common/config';
 import { UpdateBulkMarketDataDto } from '@ghostfolio/common/dtos';
 import { getCurrencyFromSymbol, isCurrency } from '@ghostfolio/common/helper';
 import { MarketDataOfMarketsResponse } from '@ghostfolio/common/interfaces';
@@ -36,7 +30,6 @@ import { getReasonPhrase, StatusCodes } from 'http-status-codes';
 @Controller('market-data')
 export class MarketDataController {
   public constructor(
-    private readonly dataProviderService: DataProviderService,
     private readonly marketDataService: MarketDataService,
     @Inject(REQUEST) private readonly request: RequestWithUser,
     private readonly symbolProfileService: SymbolProfileService,
@@ -50,39 +43,9 @@ export class MarketDataController {
     @Query('includeHistoricalData', new ParseIntPipe({ optional: true }))
     includeHistoricalData = 0
   ): Promise<MarketDataOfMarketsResponse> {
-    const [
-      marketDataFearAndGreedIndexCryptocurrencies,
-      marketDataFearAndGreedIndexStocks
-    ] = await Promise.all([
-      this.symbolService.get({
-        includeHistoricalData,
-        dataGatheringItem: {
-          dataSource: ghostfolioFearAndGreedIndexDataSourceCryptocurrencies,
-          symbol: ghostfolioFearAndGreedIndexSymbolCryptocurrencies
-        },
-        useIntradayData: true
-      }),
-      this.symbolService.get({
-        includeHistoricalData,
-        dataGatheringItem: {
-          dataSource:
-            this.dataProviderService.getDataSourceForFearAndGreedIndexStocks(),
-          symbol: ghostfolioFearAndGreedIndexSymbolStocks
-        },
-        useIntradayData: true
-      })
-    ]);
-
-    return {
-      fearAndGreedIndex: {
-        CRYPTOCURRENCIES: {
-          ...marketDataFearAndGreedIndexCryptocurrencies
-        },
-        STOCKS: {
-          ...marketDataFearAndGreedIndexStocks
-        }
-      }
-    };
+    return this.symbolService.getMarketDataOfMarkets({
+      includeHistoricalData
+    });
   }
 
   @Post(':dataSource/:symbol')
