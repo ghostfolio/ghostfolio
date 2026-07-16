3066094a57b5be0468e9cf15d28dec76db7d6c6b
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-07-16 14:26:09 +0200

    Fix removing a linked account from a buy, sell or dividend activity

--- apps/api/src/app/activities/activities.service.ts
+++ apps/api/src/app/activities/activities.service.ts
@@ -952,15 +952,15 @@ export class ActivitiesService {
 
     let isDraft = false;
 
+    if (!data.account?.connect?.id_userId?.id) {
+      data.account = { disconnect: true };
+    }
+
     if (
       ['FEE', 'INTEREST', 'LIABILITY'].includes(data.type) ||
       (data.SymbolProfile.connect.dataSource_symbol.dataSource === 'MANUAL' &&
         data.type === 'BUY')
     ) {
-      if (data.account?.connect?.id_userId?.id === null) {
-        data.account = { disconnect: true };
-      }
-
       delete data.SymbolProfile.connect;
       delete data.SymbolProfile.update.name;
     } else {
