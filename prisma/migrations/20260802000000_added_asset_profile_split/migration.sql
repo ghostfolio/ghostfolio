bca82af11e5220be4ba96acaa8cf98e0e32cfe3f
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-08-02 10:12:58 +0200

    Add AssetProfileSplit database model

---
+++ prisma/migrations/20260802000000_added_asset_profile_split/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "AssetProfileSplit" (
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "date" TIMESTAMP(3) NOT NULL,
+    "factor" DOUBLE PRECISION NOT NULL,
+    "id" TEXT NOT NULL,
+    "symbolProfileId" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "AssetProfileSplit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "AssetProfileSplit_date_idx" ON "AssetProfileSplit"("date");
+
+-- CreateIndex
+CREATE INDEX "AssetProfileSplit_symbolProfileId_idx" ON "AssetProfileSplit"("symbolProfileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AssetProfileSplit_symbolProfileId_date_key" ON "AssetProfileSplit"("symbolProfileId", "date");
+
+-- AddForeignKey
+ALTER TABLE "AssetProfileSplit" ADD CONSTRAINT "AssetProfileSplit_symbolProfileId_fkey" FOREIGN KEY ("symbolProfileId") REFERENCES "SymbolProfile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
