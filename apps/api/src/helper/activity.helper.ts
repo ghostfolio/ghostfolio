72141bdc3eab991bb0c632267a755f5562a22679
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-08-06 11:35:11 +0200

    Refactoring

--- apps/api/src/helper/activity.helper.ts
+++ apps/api/src/helper/activity.helper.ts
@@ -1,11 +1,7 @@
+import { NON_INVESTMENT_ACTIVITY_TYPES } from '@ghostfolio/common/config';
+
 import { Type as ActivityType } from '@prisma/client';
 import { endOfToday, isAfter } from 'date-fns';
-
-const ACTIVITY_TYPES_NEVER_DRAFT: ActivityType[] = [
-  'FEE',
-  'INTEREST',
-  'LIABILITY'
-];
 
 export function isDraftTagToBeAssigned({
   date,
@@ -18,7 +14,7 @@ export function isDraftTagToBeAssigned({
   storedDate?: Date;
   type: ActivityType;
 }) {
-  if (ACTIVITY_TYPES_NEVER_DRAFT.includes(type)) {
+  if (NON_INVESTMENT_ACTIVITY_TYPES.includes(type)) {
     return false;
   }
 
