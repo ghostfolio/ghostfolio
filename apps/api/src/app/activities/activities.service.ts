03ae46a2c2f91bc865b36c191f21c8b3c74bf3c5
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-08-13 21:13:50 +0200

    Refactoring

--- apps/api/src/app/activities/activities.service.ts
+++ apps/api/src/app/activities/activities.service.ts
@@ -308,8 +308,7 @@ export class ActivitiesService {
         accountId,
         userId,
         amount: amount.toNumber(),
-        currency:
-          data.currency ?? data.SymbolProfile.connectOrCreate.create.currency,
+        currency: activity.currency ?? activity.SymbolProfile.currency,
         date: data.date as Date
       });
     }
