70a4445ba0ebbae90864d304f3f2b11cdc7740a9
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-08-21 10:55:25 +0200

    Add i18n

--- apps/client/src/app/pages/resources/overview/resources-overview.component.ts
+++ apps/client/src/app/pages/resources/overview/resources-overview.component.ts
@@ -13,26 +13,22 @@ import { RouterModule } from '@angular/router';
 export class ResourcesOverviewComponent {
   public overviewItems = [
     {
-      description:
-        'Find quick answers to commonly asked questions about Ghostfolio in our Frequently Asked Questions (FAQ) section.',
+      description: $localize`Find quick answers to commonly asked questions about Ghostfolio in our Frequently Asked Questions (FAQ) section.`,
       routerLink: publicRoutes.faq.routerLink,
       title: publicRoutes.faq.title
     },
     {
-      description:
-        'Explore our guides to help you get started with investing and managing your finances.',
+      description: $localize`Explore our guides to help you get started with investing and managing your finances.`,
       routerLink: publicRoutes.resources.subRoutes.guides.routerLink,
       title: publicRoutes.resources.subRoutes.guides.title
     },
     {
-      description:
-        'Access various market resources and tools to stay informed about financial markets.',
+      description: $localize`Access various market resources and tools to stay informed about financial markets.`,
       routerLink: publicRoutes.resources.subRoutes.markets.routerLink,
       title: publicRoutes.resources.subRoutes.markets.title
     },
     {
-      description:
-        'Learn key financial terms and concepts in our comprehensive glossary.',
+      description: $localize`Learn key financial terms and concepts in our comprehensive glossary.`,
       routerLink: publicRoutes.resources.subRoutes.glossary.routerLink,
       title: publicRoutes.resources.subRoutes.glossary.title
     }
