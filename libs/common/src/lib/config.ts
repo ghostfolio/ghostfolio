baa7719eddc78430afe40b4740fc89a3b174633f
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-08-25 11:12:01 +0200

    Fix data gathering blocked by failed jobs

--- libs/common/src/lib/config.ts
+++ libs/common/src/lib/config.ts
@@ -195,7 +195,7 @@ export const E_MAIL_LINE_BREAK = '%0D%0A';
 
 export const GATHER_ASSET_PROFILE_PROCESS_JOB_NAME = 'GATHER_ASSET_PROFILE';
 export const GATHER_ASSET_PROFILE_PROCESS_JOB_OPTIONS: JobOptions = {
-  attempts: 12,
+  attempts: 6,
   backoff: {
     delay: ms('1 minute'),
     type: 'exponential'
@@ -209,7 +209,7 @@ export const GATHER_HISTORICAL_MARKET_DATA_COOLDOWN_IN_MS = ms('12 hours');
 export const GATHER_HISTORICAL_MARKET_DATA_PROCESS_JOB_NAME =
   'GATHER_HISTORICAL_MARKET_DATA';
 export const GATHER_HISTORICAL_MARKET_DATA_PROCESS_JOB_OPTIONS: JobOptions = {
-  attempts: 12,
+  attempts: 6,
   backoff: {
     delay: ms('1 minute'),
     type: 'exponential'
