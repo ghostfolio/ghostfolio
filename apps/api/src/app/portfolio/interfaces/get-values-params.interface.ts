1100d342782464bdbd31840dbcc05f48a6fc6628
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-09-05 08:49:42 +0200

    Get quotes of active holdings only

--- apps/api/src/app/portfolio/interfaces/get-values-params.interface.ts
+++ apps/api/src/app/portfolio/interfaces/get-values-params.interface.ts
@@ -1,8 +1,10 @@
 import { DataGatheringItem } from '@ghostfolio/api/services/interfaces/interfaces';
+import { AssetProfileIdentifier } from '@ghostfolio/common/interfaces';
 
 import { DateQuery } from './date-query.interface';
 
 export interface GetValuesParams {
+  assetProfileIdentifiersWithQuotes: AssetProfileIdentifier[];
   dataGatheringItems: DataGatheringItem[];
   dateQuery: DateQuery;
 }
