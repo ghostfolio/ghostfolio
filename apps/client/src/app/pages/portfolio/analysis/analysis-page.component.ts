d7b85935a7647fc3474adb2a235ff1e3fed6d731
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-09-17 17:26:52 +0200

    Add performance calculation type hint

--- apps/client/src/app/pages/portfolio/analysis/analysis-page.component.ts
+++ apps/client/src/app/pages/portfolio/analysis/analysis-page.component.ts
@@ -21,6 +21,7 @@ import type {
   GroupBy,
   ToggleOption
 } from '@ghostfolio/common/types';
+import { PerformanceCalculationType } from '@ghostfolio/common/types/performance-calculation-type.type';
 import { translate } from '@ghostfolio/ui/i18n';
 import { GfPremiumIndicatorComponent } from '@ghostfolio/ui/premium-indicator';
 import { DataService } from '@ghostfolio/ui/services';
@@ -99,6 +100,7 @@ export class GfAnalysisPageComponent implements OnInit {
     { label: $localize`Yearly`, value: 'year' }
   ];
   protected performance: PortfolioPerformance;
+  protected readonly PerformanceCalculationType = PerformanceCalculationType;
   protected performanceDataItems: HistoricalDataItem[];
   protected performanceDataItemsInPercentage: HistoricalDataItem[];
   protected readonly portfolioEvolutionDataLabel = $localize`Investment`;
