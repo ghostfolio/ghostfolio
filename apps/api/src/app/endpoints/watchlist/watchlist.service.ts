73d11b001ef0591cd266ddf97b7749eacd7039aa
Thomas Kaul <4159106+dtslvr@users.noreply.github.com>
2026-09-20 09:46:56 +0200

    Fix sorting of watchlist items without a name

--- apps/api/src/app/endpoints/watchlist/watchlist.service.ts
+++ apps/api/src/app/endpoints/watchlist/watchlist.service.ts
@@ -151,7 +151,7 @@ export class WatchlistService {
     );
 
     return watchlist.sort((a, b) => {
-      return a.name.localeCompare(b.name);
+      return (a.name ?? '').localeCompare(b.name ?? '');
     });
   }
 
